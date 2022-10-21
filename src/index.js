15d4c1816c6de201b4a833c6ca5b486d47cf80fd
Jack Golding <jack.golding@k-int.com>
2022-10-21 15:32:58 +0100

    feat: Initial switch setup
    Implemented placeholders for eventual routes within index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
+import { Suspense } from 'react';
+import { Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import SerialsManagementView from './components/SerialsManagementView';
 
-const App = ({ actAs }) => {
+import { useIntlKeyStore } from '@k-int/stripes-kint-components';
+
+const App = ({ actAs, match: { path } }) => {
+  const addKey = useIntlKeyStore((state) => state.addKey);
+  addKey('ui-oa');
+
   if (actAs === 'settings') {
     return <div>Settings go here eventually</div>;
   }
 
-  return <SerialsManagementView />;
+  return (
+    <Suspense fallback={null}>
+      <Switch>
+        <>{path}</>
+      </Switch>
+    </Suspense>
+  );
 };
 
 App.propTypes = {
   actAs: PropTypes.string.isRequired,
+  match: PropTypes.object.isRequired,
 };
 
 export default App;
