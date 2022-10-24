2de0ec69e0295c16e0b8b27a21a8f993a3966033
Jack Golding <jack.golding@k-int.com>
2022-10-24 15:45:37 +0100

    test: Fixed SerialsView test
    Fixed issue in which serials view test was failing

--- src/components/views/SerialsView/SerialsView.test.js
+++ src/components/views/SerialsView/SerialsView.test.js
@@ -3,9 +3,17 @@ import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
 import SerialsView from './SerialsView';
 
+const onClose = jest.fn();
+
 describe('SerialsView', () => {
   test('renders expected SerialsView', () => {
-    renderWithIntl(<SerialsView />);
+    renderWithIntl(
+      <SerialsView
+        onClose={onClose}
+        queryProps={{ isLoading: false }}
+        resource={{}}
+      />
+    );
     // Filler test for intial repo setup
     expect(1).toBe(1);
   });
