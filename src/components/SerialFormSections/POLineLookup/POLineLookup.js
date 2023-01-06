3bd7a67959e32d9db979f21d71f6b8cf31a16ec5
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2023-01-06 17:01:16 +0000

    feat: PO Line Lookup (#29)
    * feat: PO Line lookup framework
    
    Added framework for PO Line lookup component within serial record form
    
    * feat: PO Line storage
    
    PO Line now correctly stores selected resource
    
    * chore: Updated translations
    
    * refactor: PO Line field
    
    Refactored PO Line lookup field to allow for card to be passed as children to POLineLookup
    
    * feat: PO Line card info
    
    Added information to PO line card when a PO Line has been selected

---
+++ src/components/SerialFormSections/POLineLookup/POLineLookup.js
@@ -0,0 +1,117 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import { FormattedMessage } from 'react-intl';
+
+import { Button, Card, Layout, Tooltip } from '@folio/stripes/components';
+
+import { AppIcon, Pluggable } from '@folio/stripes/core';
+
+const propTypes = {
+  children: PropTypes.node,
+  disabled: PropTypes.bool,
+  id: PropTypes.string,
+  input: PropTypes.shape({
+    name: PropTypes.string,
+    value: PropTypes.string,
+  }),
+  onResourceSelected: PropTypes.func,
+  resource: PropTypes.object,
+};
+
+const POLineLookup = ({
+  children,
+  disabled,
+  id,
+  input: { name, value },
+  onResourceSelected,
+  resource,
+}) => {
+  let triggerButton = useRef(null);
+
+  const renderPOLineLinkButton = () => (
+    <Pluggable
+      addLines={onResourceSelected}
+      dataKey="find-po-line"
+      isSingleSelect
+      renderTrigger={(pluggableRenderProps) => {
+        triggerButton = pluggableRenderProps.buttonRef;
+
+        const buttonProps = {
+          'aria-haspopup': 'true',
+          buttonRef: triggerButton,
+          buttonStyle: value ? 'default' : 'primary',
+          disabled,
+          id: `${id}-find-po-line-btn`,
+          marginBottom0: true,
+          name,
+          onClick: pluggableRenderProps.onClick,
+        };
+
+        if (value) {
+          return (
+            <Tooltip
+              id={`${id}-po-line-button-tooltip`}
+              text={
+                <FormattedMessage
+                  id="ui-serials-management.poLine.replacePOLineSpecific"
+                  values={{ titleOrPackage: resource?.titleOrPackage }}
+                />
+              }
+              triggerRef={triggerButton}
+            >
+              {({ ariaIds }) => (
+                <Button aria-labelledby={ariaIds.text} {...buttonProps}>
+                  <FormattedMessage id="ui-serials-management.poLine.replacePOLine" />
+                </Button>
+              )}
+            </Tooltip>
+          );
+        }
+        return (
+          <Button {...buttonProps}>
+            <FormattedMessage id="ui-serials-management.poLine.linkPOLine" />
+          </Button>
+        );
+      }}
+      type="find-po-line"
+    >
+      <FormattedMessage id="ui-serials-management.poLine.noPlugin" />
+    </Pluggable>
+  );
+
+  const renderEmpty = () => (
+    <>
+      <Layout className="textCentered">
+        <strong>
+          <FormattedMessage id="ui-serials-management.poLine.noneLinked" />
+        </strong>
+      </Layout>
+      <Layout className="textCentered">
+        <FormattedMessage id="ui-serials-management.poLine.linkToStart" />
+      </Layout>
+    </>
+  );
+
+  return (
+    <Card
+      cardStyle={value ? 'positive' : 'negative'}
+      headerEnd={renderPOLineLinkButton()}
+      headerStart={
+        <AppIcon app="orders" size="small">
+          <strong>
+            <FormattedMessage id="ui-serials-management.poLine" />
+          </strong>
+        </AppIcon>
+      }
+      id={id}
+      roundedBorder
+    >
+      {value ? children : renderEmpty()}
+    </Card>
+  );
+};
+
+POLineLookup.propTypes = propTypes;
+
+export default POLineLookup;
