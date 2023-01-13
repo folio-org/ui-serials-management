e8d0b8b283d693e96dd1eb2ba39744e640693c7b
Jack Golding <jack.golding@k-int.com>
2023-01-13 10:04:59 +0000

    feat: Create serial route
    Added route and endpoint for creating new serial records

--- src/routes/SerialCreateRoute/SerialCreateRoute.js
+++ src/routes/SerialCreateRoute/SerialCreateRoute.js
@@ -1,28 +1,47 @@
 import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 
+import { useMutation } from 'react-query';
 import { useHistory, useLocation } from 'react-router-dom';
 
+import { useOkapiKy } from '@folio/stripes/core';
+
 import { SerialForm } from '../../components/views';
+import { SERIALS_ENDPOINT } from '../../constants/endpoints';
 import urls from '../../components/utils/urls';
 
 const SerialCreateRoute = () => {
   const history = useHistory();
   const location = useLocation();
+  const ky = useOkapiKy();
 
   const handleClose = () => {
     history.push(`${urls.serials()}${location.search}`);
   };
 
+  const { mutateAsync: postSerial } = useMutation(
+    ['ui-serials-management', 'SerialCreateRoute', 'postSerial'],
+    (data) => {
+      ky.post(SERIALS_ENDPOINT, { json: data })
+        .json()
+        .then(() => handleClose());
+    }
+  );
+
+  const submitSerial = async (values) => {
+    const submitValues = { orderLine: { remoteId: values?.orderLine?.id } };
+    await postSerial(submitValues);
+  };
+
   return (
     <>
-      <Form mutators={arrayMutators} onSubmit={handleClose}>
+      <Form mutators={arrayMutators} onSubmit={submitSerial}>
         {({ handleSubmit }) => (
           <form onSubmit={handleSubmit}>
             <SerialForm
               handlers={{
                 onClose: handleClose,
-                onSubmit: handleClose,
+                onSubmit: handleSubmit,
               }}
             />
           </form>
