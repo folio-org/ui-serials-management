fcda0cf536e0189f855870f5fa31d9885fa7306e
Jack Golding <jack.golding@k-int.com>
2023-01-18 16:56:46 +0000

    feat: Serial notes
    Added field array for serial notes, may need some refactoring for consistency in the future

--- src/components/SerialFormSections/index.js
+++ src/components/SerialFormSections/index.js
@@ -1,3 +1,4 @@
 export { default as POLineForm } from './POLineForm';
 export { default as POLineLookup } from './POLineLookup';
 export { default as SerialInfoForm } from './SerialInfoForm';
+export { default as SerialNoteFieldArray } from './SerialNoteFieldArray';
