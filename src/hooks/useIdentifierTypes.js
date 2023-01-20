bd40e1c1f797cffb07a543761719e50a6633e154
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2023-01-20 15:52:49 +0000

    fix: IdentifierType/AcqUnit hook (#51)
    * fix: useIdentifier enabled flag
    
    Fixed enabled flag to be correctly true/false based on type ids length as opposed to just existing, causing invalid fetches
    
    * perf: Unique identifierTypeId array
    
    identiferTypeId array passed to identifier-type query is now unique, removing redundant duplicate ids within query string
    
    * fix: Hook query keys
    
    Added unique hook query keys to identifierType and acqUnit hooks to ensure fetch is refired each time the parameters passed are changed

--- src/hooks/useIdentifierTypes.js
+++ src/hooks/useIdentifierTypes.js
@@ -7,18 +7,23 @@ import { IDENTIFIER_TYPES_ENDPOINT } from '../constants/endpoints';
 export const useIdentifierTypes = (identifierTypeIds) => {
   const ky = useOkapiKy();
 
+  const uniqueIdsArray = [...new Set(identifierTypeIds)];
+
   const queryString =
-    'id==(' + identifierTypeIds?.map((e) => `"${e}"`).join(' or ') + ')';
+    'id==(' + uniqueIdsArray?.map((e) => `"${e}"`).join(' or ') + ')';
+
+  // Using query string within query keys to ensure it is fired when acqUnits are changed
+  // May be a better alternative for acqUnitIds specified query keys
 
   const { isLoading, data = [] } = useQuery(
-    ['ui-serials-management', IDENTIFIER_TYPES_ENDPOINT],
+    ['ui-serials-management', IDENTIFIER_TYPES_ENDPOINT, queryString],
     () => ky.get(`${IDENTIFIER_TYPES_ENDPOINT}?query=${queryString}`).json(),
-    { enabled: !!identifierTypeIds }
+    { enabled: !!identifierTypeIds?.length }
   );
 
   return {
     isLoading,
-    data: data?.identifierTypes,
+    data: data?.identifierTypes || [],
   };
 };
 
