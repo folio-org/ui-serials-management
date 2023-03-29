daa0d143ae60387d3efd553c7cc6fb02ec229a5f
Jack Golding <jack.golding@k-int.com>
2023-03-29 14:28:35 +0100

    feat: Omissions field array first draft
    First draft of omissions field array

--- src/components/RulesetFormSections/OmissionField/OmissionField.js
+++ src/components/RulesetFormSections/OmissionField/OmissionField.js
@@ -4,13 +4,10 @@ import { Field, useFormState } from 'react-final-form';
 
 import {
   Select,
-  Button,
   Col,
-  IconButton,
   TextField,
   Row,
-  Label,
-  Tooltip,
+  MultiSelection,
 } from '@folio/stripes/components';
 
 import {
@@ -18,10 +15,42 @@ import {
   composeValidators,
 } from '@folio/stripes-erm-components';
 
-import { validateWithinRange, validateWholeNumber } from '../../utils';
+import {
+  SORTED_MONTHS,
+  SORTED_WEEKDAYS,
+} from '../../../constants/sortedArrays';
 
-const OmissionsField = ({ name, index, omission }) => {
+import {
+  validateWithinRange,
+  validateWholeNumber,
+  useSerialsManagementRefdata,
+  selectifyRefdata,
+} from '../../utils';
+
+const [MONTHS, WEEKDAYS] = ['Global.Month', 'Global.Weekday'];
+
+const OmissionsField = ({ name, index }) => {
   const { values } = useFormState();
+  const refdataValues = useSerialsManagementRefdata([MONTHS, WEEKDAYS]);
+
+  const renderIssueField = (minValue = 1, maxValue = 1) => {
+    return (
+      <Field
+        component={TextField}
+        label={
+          <FormattedMessage id="ui-serials-management.omissions.issue" />
+        }
+        name={`${name}[${index}].issue`}
+        required
+        type="number"
+        validate={composeValidators(
+          requiredValidator,
+          validateWithinRange(minValue, maxValue),
+          validateWholeNumber
+        )}
+      />
+    );
+  };
 
   const renderDayField = (minValue, maxValue) => {
     return (
@@ -40,32 +69,123 @@ const OmissionsField = ({ name, index, omission }) => {
     );
   };
 
+  const renderMonthField = (labelId) => {
+    return (
+      <Field
+        component={Select}
+        dataOptions={[
+          { value: '', label: '' },
+          ...selectifyRefdata(refdataValues, MONTHS, 'value').sort((a, b) => {
+            return (
+              SORTED_MONTHS.indexOf(a.value) - SORTED_MONTHS.indexOf(b.value)
+            );
+          }),
+        ]}
+        label={<FormattedMessage id={labelId} />}
+        name={`${name}[${index}].month.value`}
+        required
+        validate={requiredValidator}
+      />
+    );
+  };
+
+  const renderMonthsField = () => {
+    return (
+      <Field
+        component={MultiSelection}
+        dataOptions={[
+          ...selectifyRefdata(refdataValues, MONTHS, 'value').sort((a, b) => {
+            return (
+              SORTED_MONTHS.indexOf(a.value) - SORTED_MONTHS.indexOf(b.value)
+            );
+          }),
+        ]}
+        label={<FormattedMessage id="ui-serials-management.omissions.months" />}
+        name={`${name}[${index}].months`}
+        renderToOverlay
+        required
+        validate={requiredValidator}
+      />
+    );
+  };
+
+  const renderWeeksField = (minValue, maxValue, labelId) => {
+    const dataOptions = [];
+    for (let i = minValue; i <= maxValue; i++) {
+      dataOptions.push({ value: i, label: i });
+    }
+    return (
+      <Field
+        component={MultiSelection}
+        dataOptions={[...dataOptions]}
+        label={<FormattedMessage id={labelId} />}
+        name={`${name}[${index}].weeks`}
+        renderToOverlay
+        required
+        validate={requiredValidator}
+      />
+    );
+  };
+
+  const renderWeekdayField = () => {
+    return (
+      <Field
+        component={MultiSelection}
+        dataOptions={[
+          ...selectifyRefdata(refdataValues, WEEKDAYS, 'value').sort((a, b) => {
+            return (
+              SORTED_WEEKDAYS.indexOf(a.value) -
+              SORTED_WEEKDAYS.indexOf(b.value)
+            );
+          }),
+        ]}
+        label={
+          <FormattedMessage id="ui-serials-management.omissions.weekdays" />
+        }
+        name={`${name}[${index}].pattern.weekday.value`}
+        renderToOverlay
+        required
+        validate={requiredValidator}
+      />
+    );
+  };
+
   const omissionTypeFormats = {
     daysInMonth: {
-      fields: [renderDayField(1, 31)],
+      fields: [
+        renderDayField(1, 31),
+        renderMonthField('ui-serials-management.recurrence.ofMonth'),
+      ],
     },
-    // weekdaysInWeek: {
-    //   fields: [renderWeekdayField()],
-    // },
-    // weekdaysInMonth: {
-    //   fields: [renderDayField(false, 1, 31)],
-    // },
-    // weeks: {
-    //   fields: [renderWeekdayField(), renderWeekField(false, 1, 4)],
-    // },
-    // weeksInEveryMonth: {
-    //   fields: [renderDayField(false, 1, 31), renderMonthField()],
-    // },
-    // months: {
-    //   fields: [renderWeekdayField(), renderWeekField(false, 1, 52)],
-    // },
-    // nthIssue: {
-    //   fields: [
-    //     renderWeekdayField(),
-    //     renderWeekField(false, 1, 4),
-    //     renderMonthField(),
-    //   ],
-    // },
+    weekdaysInWeek: {
+      fields: [
+        renderWeekdayField(),
+        renderWeeksField(1, 52, 'ui-serials-management.omissions.inWeeks'),
+      ],
+    },
+    weekdaysInMonth: {
+      fields: [
+        renderWeekdayField(),
+        renderMonthField('ui-serials-management.omissions.inMonth'),
+      ],
+    },
+    weeks: {
+      fields: [
+        renderWeeksField(1, 52, 'ui-serials-management.omissions.weeks'),
+      ],
+    },
+    weeksInEveryMonth: {
+      fields: [renderWeeksField(1, 4, 'ui-serials-management.omissions.weeks')],
+    },
+    months: {
+      fields: [renderMonthsField()],
+    },
+    nthIssue: {
+      fields: [
+        renderIssueField(1, values?.recurrence?.issues),
+        renderMonthField('ui-serials-management.recurrence.ofMonth'),
+      ],
+    },
   };
 
   return (
@@ -77,6 +197,12 @@ const OmissionsField = ({ name, index, omission }) => {
             dataOptions={[
               { value: '', label: '' },
               { value: 'daysInMonth', label: 'Days in month' },
+              { value: 'weekdaysInWeek', label: 'Weekdays in week' },
+              { value: 'weekdaysInMonth', label: 'Weekdays in month' },
+              { value: 'weeks', label: 'Weeks' },
+              { value: 'weeksInEveryMonth', label: 'Weeks in every month' },
+              { value: 'months', label: 'Months' },
+              { value: 'nthIssue', label: 'Nth issue' },
             ]}
             label={
               <FormattedMessage id="ui-serials-management.omissions.omissionType" />
@@ -99,7 +225,6 @@ const OmissionsField = ({ name, index, omission }) => {
 OmissionsField.propTypes = {
   name: PropTypes.string,
   index: PropTypes.string,
-  omission: PropTypes.object,
 };
 
 export default OmissionsField;
