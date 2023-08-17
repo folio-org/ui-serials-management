2d51169c16591e49a64c6581178f93e2744493e4
Jack Golding <jack.golding@k-int.com>
2023-08-17 16:44:18 +0100

    feat: Deprecated publication patterns
    Added deprecated publication patterns to the serial view pane
    
    UISER-33

---
+++ src/components/SerialSections/DeprecatedPublicationPatterns/DeprecatedPublicationPatterns.js
@@ -0,0 +1,79 @@
+/* eslint-disable react/style-prop-object */
+import PropTypes from 'prop-types';
+import { FormattedMessage, FormattedDate } from 'react-intl';
+
+import {
+  Accordion,
+  Badge,
+  Col,
+  Row,
+  MultiColumnList,
+} from '@folio/stripes/components';
+
+
+const proptypes = {
+  serial: PropTypes.object,
+};
+
+const DeprecatedPublicationPatterns = ({ serial }) => {
+  const deprecatedRulesets = serial?.serialRulesets?.filter(
+    (sr) => sr?.rulesetStatus?.value === 'deprecated'
+  );
+
+  const formatter = {
+    patternId: (e) => {
+      return e.rulesetNumber;
+    },
+    lastUpdated: (e) => {
+      return <FormattedDate value={e?.lastUpdated} />;
+    },
+    description: (e) => {
+      return e?.description;
+    },
+  };
+
+  const renderBadge = () => {
+    return <Badge>{deprecatedRulesets?.length}</Badge>;
+  };
+
+  return (
+    <Accordion
+      closedByDefault
+      displayWhenClosed={renderBadge()}
+      displayWhenOpen={renderBadge()}
+      label={
+        <FormattedMessage id="ui-serials-management.ruleset.deprecatedPublicationPatterns" />
+      }
+    >
+      {!!deprecatedRulesets?.length && (
+        <>
+          <Row>
+            <Col xs={12}>
+              <MultiColumnList
+                columnMapping={{
+                  patternId: (
+                    <FormattedMessage id="ui-serials-management.ruleset.patternId" />
+                  ),
+                  lastUpdated: (
+                    <FormattedMessage id="ui-serials-management.lastUpdated" />
+                  ),
+                  description: (
+                    <FormattedMessage id="ui-serials-management.ruleset.patternDescription" />
+                  ),
+                }}
+                contentData={deprecatedRulesets}
+                formatter={formatter}
+                interactive={false}
+                visibleColumns={['patternId', 'lastUpdated', 'description']}
+              />
+            </Col>
+          </Row>
+        </>
+      )}
+    </Accordion>
+  );
+};
+
+DeprecatedPublicationPatterns.propTypes = proptypes;
+
+export default DeprecatedPublicationPatterns;
