3c3d02e13fd097dce3c4dd56b08c831338e1e9d6
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2024-01-29 16:14:46 +0000

    feat: Removed patterns tab (#144)
    Removed pattern tab and view from SASQ route switchers and views

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,4 +3,3 @@ export { default as SerialCreateRoute } from './SerialCreateRoute';
 export { default as RulesetCreateRoute } from './RulesetCreateRoute';
 export { default as SerialEditRoute } from './SerialEditRoute';
 export { default as PieceSetsRoute } from './PieceSetsRoute';
-export { default as PatternsRoute } from './PatternsRoute';
