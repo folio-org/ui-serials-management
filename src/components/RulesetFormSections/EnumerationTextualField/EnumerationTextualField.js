214acdb2c74ba0a3fa075fad62b92455fbe49637
Jack Golding <jack.golding@k-int.com>
2024-03-12 15:30:28 +0000

    fix: Enumeration textual field tweaks
    Added required and whole number valdation to units field and added aria labels to fields
    
    UISER-69

--- src/components/RulesetFormSections/EnumerationTextualField/EnumerationTextualField.js
+++ src/components/RulesetFormSections/EnumerationTextualField/EnumerationTextualField.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-
+import { FormattedMessage } from 'react-intl';
 import { Field } from 'react-final-form';
 
 import {
@@ -9,8 +9,12 @@ import {
   IconButton,
   Select,
 } from '@folio/stripes/components';
+import {
+  composeValidators,
+  requiredValidator,
+} from '@folio/stripes-erm-components';
 
-import { requiredValidator } from '@folio/stripes-erm-components';
+import { validateWholeNumber } from '../../utils';
 
 const EnumerationTextualField = ({
   items,
@@ -24,20 +28,47 @@ const EnumerationTextualField = ({
     <Row>
       <Col xs={1}>{index + 1}</Col>
       <Col xs={2}>
-        <Field component={TextField} name={`${name}.units`} />
+        <FormattedMessage id="ui-serials-management.ruleset.numberOfIssues">
+          {(ariaLabel) => (
+            <Field
+              aria-label={ariaLabel}
+              component={TextField}
+              name={`${name}.units`}
+              required
+              type="number"
+              validate={composeValidators(
+                requiredValidator,
+                validateWholeNumber
+              )}
+            />
+          )}
+        </FormattedMessage>
       </Col>
       <Col xs={2}>
-        <Field
-          component={Select}
-          dataOptions={[{ label: '', value: '' }, ...dataOptions]}
-          disabled={!dataOptions}
-          name={`${name}.value`}
-          required
-          validate={requiredValidator}
-        />
+        <FormattedMessage id="ui-serials-management.ruleset.labelText">
+          {(ariaLabel) => (
+            <Field
+              aria-label={ariaLabel}
+              component={Select}
+              dataOptions={[{ label: '', value: '' }, ...dataOptions]}
+              disabled={!dataOptions}
+              name={`${name}.value`}
+              required
+              validate={requiredValidator}
+            />
+          )}
+        </FormattedMessage>
       </Col>
       <Col xs={3}>
-        <Field component={TextField} name={`${name}.internalNote`} />
+        <FormattedMessage id="ui-serials-management.ruleset.internalNote">
+          {(ariaLabel) => (
+            <Field
+              aria-label={ariaLabel}
+              component={TextField}
+              name={`${name}.internalNote`}
+            />
+          )}
+        </FormattedMessage>
       </Col>
       {items?.length > 1 && (
         <Col xs={2}>
