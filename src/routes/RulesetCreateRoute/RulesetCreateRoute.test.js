87da49acc52e3e1215e1a19425379d49bd26bb3b
MonireRasouli <monireh.rasouli@k-int.co.uk>
2024-03-18 08:11:47 +0000

    test coverage, refs, UISER-51

--- src/routes/RulesetCreateRoute/RulesetCreateRoute.test.js
+++ src/routes/RulesetCreateRoute/RulesetCreateRoute.test.js
@@ -1,48 +1,11 @@
 import { renderWithIntl } from '@folio/stripes-erm-testing';
 import { MemoryRouter } from 'react-router-dom';
 
-import RulesetCreateRoute from './RulesetCreateRoute';
-
 import { translationsProperties } from '../../../test/helpers';
 
+import RulesetCreateRoute from './RulesetCreateRoute';
+
 jest.mock('../../components/views/RulesetForm', () => () => <div>RulesetForm</div>);
-
-const history = {
-  length: 12,
-  action: 'POP',
-  location: {
-    pathname:
-      '/serials-management/serials/db9da0f4-ee0c-4b29-93a2-25a0508b4155/rulesets/create',
-    search: '',
-    hash: '',
-    key: 'ca402w',
-  },
-  createHref: () => {},
-  push: () => {},
-  replace: () => {},
-  go: () => {},
-  goBack: () => {},
-  goForward: () => {},
-  block: () => {},
-  listen: () => {},
-};
-
-const location = {
-  pathname:
-    '/serials-management/serials/db9da0f4-ee0c-4b29-93a2-25a0508b4155/rulesets/create',
-  search: '',
-  hash: '',
-  key: 'ca402w',
-};
-
-const match = {
-  path: '/serials-management/serials/:id/rulesets/create',
-  url: '/serials-management/serials/db9da0f4-ee0c-4b29-93a2-25a0508b4155/rulesets/create',
-  isExact: true,
-  params: {
-    id: 'db9da0f4-ee0c-4b29-93a2-25a0508b4155',
-  },
-};
 
 let renderComponent;
 
@@ -50,11 +13,12 @@ describe('RulesetCreateRoute', () => {
   beforeEach(() => {
     renderComponent = renderWithIntl(
       <MemoryRouter>
-        <RulesetCreateRoute history={history} location={location} match={match} />
+        <RulesetCreateRoute />
       </MemoryRouter>,
       translationsProperties
     );
   });
+
   test('renders the RulesetForm component', () => {
     const { getByText } = renderComponent;
     expect(getByText('RulesetForm')).toBeInTheDocument();
