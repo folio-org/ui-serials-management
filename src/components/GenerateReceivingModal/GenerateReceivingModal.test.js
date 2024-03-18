b2538884b22cd594a56fa04d0b1d87d228234b80
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-03-18 14:47:31 +0000

    test: Updating test coverage for GenerateReceivingPieces
    WIP -- update test coverage for GenerateReceivingPieces component

--- src/components/GenerateReceivingModal/GenerateReceivingModal.test.js
+++ src/components/GenerateReceivingModal/GenerateReceivingModal.test.js
@@ -1,46 +1,58 @@
-import { waitFor } from '@folio/jest-config-stripes/testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { useState } from 'react';
+import { waitFor, screen } from '@folio/jest-config-stripes/testing-library/react';
 
 import {
   renderWithIntl,
-  TestForm,
   Button,
   KeyValue,
   Select,
   Checkbox,
+  TextField,
+  Modal,
 } from '@folio/stripes-erm-testing';
 
 import GenerateReceivingModal from './GenerateReceivingModal';
 
 import { translationsProperties } from '../../../test/helpers';
-import { pieceSet, serial } from '../../../test/resources';
+import {
+  holdings as mockHoldings,
+  locations as mockLocations,
+  pieceSet,
+  serial
+} from '../../../test/resources';
 
-const onSubmit = jest.fn();
-const setShowModal = jest.fn();
+const TestComponent = () => {
+  // We need actual state in here for close test
+  const [showModal, setShowModal] = useState(true);
+
+  return (
+    <GenerateReceivingModal
+      holdingIds={null}
+      pieceSet={pieceSet}
+      serial={serial}
+      setShowModal={setShowModal}
+      showModal={showModal}
+    />
+  );
+};
+
+jest.mock('../../hooks', () => ({
+  ...jest.requireActual('../../hooks'),
+  useLocations: () => ({ isLoading: false, data: mockLocations }),
+  useHoldings: () => ({ isLoading: false, data: mockHoldings }),
+}));
 
 let renderComponent;
 describe('GenerateReceivingModal', () => {
   beforeEach(() => {
     renderComponent = renderWithIntl(
-      <MemoryRouter>
-        <TestForm onSubmit={onSubmit}>
-          <GenerateReceivingModal
-            holdingIds={null}
-            pieceSet={pieceSet}
-            serial={serial}
-            setShowModal={setShowModal}
-            showModal
-          />
-        </TestForm>
-        ,
-      </MemoryRouter>,
+      <TestComponent />,
       translationsProperties
     );
   });
 
-  test('renders the modal headline', async () => {
-    const { getAllByText } = renderComponent;
-    waitFor(() => expect(getAllByText('Generate receiving pieces')).toBeInTheDocument());
+  test('renders the modal', async () => {
+    await Modal('Generate receiving pieces').exists();
   });
 
   test('renders the expected Total pieces value', async () => {
@@ -89,7 +101,7 @@ describe('GenerateReceivingModal', () => {
     Select('Piece format').choose('Physical');
   });
 
-  test('clicking the Supplement checkbox', async () => {
+  test('renders the Supplement checkbox', async () => {
     await Checkbox('Supplement').is({ checked: false });
   });
 
@@ -99,10 +111,46 @@ describe('GenerateReceivingModal', () => {
   });
 
   test('renders the Generate receiving pieces button', async () => {
-    await Button('Generate receiving pieces').has({ disabled: true });
+    await Button({ id: 'generate-recieving-pieces-button' }).has({ disabled: true });
   });
 
-  test('Close button should be enabeled', async () => {
+  describe('filling out required fields', () => {
+    beforeEach(async () => {
+      await waitFor(async () => {
+        await TextField('Time between publication and receipt (days)*').fillIn('0');
+        await Select('Location*').choose(mockLocations[0].name);
+      });
+    });
+
+    test('renders the Generate receiving pieces button', async () => {
+      await Button({ id: 'generate-recieving-pieces-button' }).has({ disabled: false });
+    });
+
+    describe('checking the supplement checkbox', () => {
+      beforeEach(async () => {
+        await waitFor(async () => {
+          await Checkbox('Supplement').click();
+        });
+      });
+
+      test('supplement checkbox is now checked', async () => {
+        await Checkbox('Supplement').is({ checked: true });
+      });
+    });
+  });
+
+  test('Close button should be enabled', async () => {
     await Button('Close').has({ disabled: false });
+  });
+
+  describe('Closing modal', () => {
+    beforeEach(async () => {
+      await Button('Close').click();
+    });
+
+    test('does not render the modal', async () => {
+      screen.debug();
+      await Modal('Generate receiving pieces').absent();
+    });
   });
 });
---
+++ test/resources/holdings.js
@@ -0,0 +1,334 @@
+import locations from './locations';
+
+export default [
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: '65032151-39a5-4cef-8810-5350eb316300',
+    _version: 1,
+    hrid: 'hold000000000006',
+    formerIds: [],
+    instanceId: 'f31a36de-fcf8-44f9-87ef-a55d06ad21ae',
+    permanentLocationId: locations[3].id,
+    effectiveLocationId: locations[3].id,
+    electronicAccess: [],
+    callNumber: 'MCN FICTION',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [
+      {
+        statement: 'Line 1b'
+      },
+      {
+        statement: 'Line 2b'
+      }
+    ],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.749+00:00',
+      updatedDate: '2024-03-18T01:52:01.749+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: 'e6d7e91a-4dbc-4a70-9b38-e000d2fbdc79',
+    _version: 1,
+    hrid: 'hold000000000010',
+    formerIds: [],
+    instanceId: 'cf23adf0-61ba-4887-bf82-956c4aae2260',
+    permanentLocationId: locations[2].id,
+    effectiveLocationId: locations[2].id,
+    electronicAccess: [],
+    callNumber: 'some-callnumber',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [
+      {
+        statement: 'Line 1b'
+      },
+      {
+        statement: 'Line 2b'
+      }
+    ],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.746+00:00',
+      updatedDate: '2024-03-18T01:52:01.746+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: '0c45bb50-7c9b-48b0-86eb-178a494e25fe',
+    _version: 1,
+    hrid: 'hold000000000002',
+    formerIds: [
+      'ABW4508',
+      '442882'
+    ],
+    instanceId: '69640328-788e-43fc-9c3c-af39e243f3b7',
+    permanentLocationId: locations[2].id,
+    effectiveLocationId: locations[2].id,
+    electronicAccess: [
+      {
+        uri: 'http://www.ebscohost.com',
+        materialsSpecification: '1984-',
+        relationshipId: '3b430592-2e09-4b48-9a0c-0636d66b9fb3'
+      },
+      {
+        uri: 'http://www.jstor.com',
+        materialsSpecification: '1984-',
+        publicNote: 'Most recent 4 years not available.',
+        relationshipId: '3b430592-2e09-4b48-9a0c-0636d66b9fb3'
+      }
+    ],
+    callNumber: 'K1 .M44',
+    acquisitionMethod: 'Purchase',
+    receiptStatus: 'Not currently received',
+    administrativeNotes: [],
+    notes: [
+      {
+        holdingsNoteTypeId: 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        note: ' Subscription cancelled per Evans Current Periodicals Selector Review. acq',
+        staffOnly: true
+      },
+      {
+        holdingsNoteTypeId: 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        note: 'Asked Ebsco to check with publisher and ask what years were paid since we are missing (2001:Oct.-Dec.), (All of 2002), & (2003:Jan.-Feb.). 20030305. evaldez',
+        staffOnly: false
+      },
+      {
+        holdingsNoteTypeId: 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        note: 'Backorder:v.87(2001:Oct.-Dec)-v.88(2002). eluza',
+        staffOnly: false
+      },
+      {
+        holdingsNoteTypeId: 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        note: 'WITH 2010 TREAT ISSUE S AS DISCARDS. dgill',
+        staffOnly: false
+      }
+    ],
+    retentionPolicy: 'Permanently retained.',
+    holdingsStatements: [
+      {
+        statement: 'v.70-84 (1984-1998)'
+      },
+      {
+        statement: 'v.85:no. 1-11 (1999:Jan.-Nov.)'
+      },
+      {
+        statement: 'v.87:no.1-9 (2001:Jan.-Sept.)'
+      },
+      {
+        statement: 'v.89:no.2-12 (2003:Feb.-Dec.)'
+      },
+      {
+        statement: 'v.90-95 (2004-2009)'
+      }
+    ],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [
+      {
+        statement: 'no.1-23 '
+      }
+    ],
+    copyNumber: '1',
+    statisticalCodeIds: [
+      '775b6ad4-9c35-4d29-bf78-8775a9b42226'
+    ],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.727+00:00',
+      updatedDate: '2024-03-18T01:52:01.727+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: '55f48dc6-efa7-4cfe-bc7c-4786efe493e3',
+    _version: 1,
+    hrid: 'hold000000000012',
+    formerIds: [],
+    instanceId: 'bbd4a5e1-c9f3-44b9-bfdf-d184e04f0ba0',
+    permanentLocationId: locations[5].id,
+    effectiveLocationId: locations[5].id,
+    electronicAccess: [],
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.786+00:00',
+      updatedDate: '2024-03-18T01:52:01.786+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: '67cd0046-e4f1-4e4f-9024-adf0b0039d09',
+    _version: 1,
+    hrid: 'hold000000000007',
+    formerIds: [],
+    instanceId: 'a89eccf0-57a6-495e-898d-32b9b2210f2f',
+    permanentLocationId: locations[5].id,
+    effectiveLocationId: locations[5].id,
+    electronicAccess: [],
+    callNumber: 'D15.H63 A3 2002',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [
+      {
+        statement: 'Line 1b'
+      },
+      {
+        statement: 'Line 2b'
+      }
+    ],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.787+00:00',
+      updatedDate: '2024-03-18T01:52:01.787+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: '65cb2bf0-d4c2-4886-8ad0-b76f1ba75d61',
+    _version: 1,
+    hrid: 'hold000000000004',
+    formerIds: [],
+    instanceId: '7fbd5d84-62d1-44c6-9c45-6cb173998bbd',
+    permanentLocationId: locations[2].id,
+    effectiveLocationId: locations[2].id,
+    electronicAccess: [],
+    callNumber: 'PR6056.I4588 B749 2016',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.788+00:00',
+      updatedDate: '2024-03-18T01:52:01.788+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: 'fb7b70f1-b898-4924-a991-0e4b6312bb5f',
+    _version: 2,
+    hrid: 'hold000000000005',
+    formerIds: [],
+    instanceId: '7fbd5d84-62d1-44c6-9c45-6cb173998bbd',
+    permanentLocationId: locations[0].id,
+    effectiveLocationId: locations[0].id,
+    electronicAccess: [],
+    callNumber: 'PR6056.I4588 B749 2016',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    discoverySuppress: true,
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.742+00:00',
+      updatedDate: '2024-03-18T04:52:01.574+00:00',
+      updatedByUserId: '2c32c43d-3963-4b35-be32-95c1ac9eddf9'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: 'e9285a1c-1dfc-4380-868c-e74073003f43',
+    _version: 1,
+    hrid: 'hold000000000011',
+    formerIds: [],
+    instanceId: 'e54b1f4d-7d05-4b1a-9368-3c36b75d8ac6',
+    permanentLocationId: locations[2].id,
+    effectiveLocationId: locations[2].id,
+    electronicAccess: [],
+    callNumber: 'M1366.S67 T73 2017',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [
+      {
+        statement: 'Line 1b'
+      },
+      {
+        statement: 'Line 2b'
+      }
+    ],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.799+00:00',
+      updatedDate: '2024-03-18T01:52:01.799+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: '68872d8a-bf16-420b-829f-206da38f6c10',
+    _version: 1,
+    hrid: 'hold000000000008',
+    formerIds: [],
+    instanceId: '6506b79b-7702-48b2-9774-a1c538fdd34e',
+    permanentLocationId: locations[2].id,
+    effectiveLocationId: locations[2].id,
+    electronicAccess: [],
+    callNumber: 'some-callnumber',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [
+      {
+        statement: 'Line 1b'
+      },
+      {
+        statement: 'Line 2b'
+      }
+    ],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:01.811+00:00',
+      updatedDate: '2024-03-18T01:52:01.811+00:00'
+    }
+  },
+  {
+    holdingsItems: [],
+    bareHoldingsItems: [],
+    id: '13767c78-f8d0-425e-801d-cc5bd475856a',
+    _version: 1,
+    sourceId: 'f32d531e-df79-46b3-8932-cdd35f7a2264',
+    hrid: 'bwho000000001',
+    holdingsTypeId: '03c9c400-b9e3-4a07-ac0e-05ab470233ed',
+    formerIds: [],
+    instanceId: 'ce9dd893-c812-49d5-8973-d55d018894c4',
+    permanentLocationId: locations[2].id,
+    effectiveLocationId: locations[2].id,
+    electronicAccess: [],
+    callNumber: 'DE3',
+    administrativeNotes: [],
+    notes: [],
+    holdingsStatements: [],
+    holdingsStatementsForIndexes: [],
+    holdingsStatementsForSupplements: [],
+    statisticalCodeIds: [],
+    metadata: {
+      createdDate: '2024-03-18T01:52:02.050+00:00',
+      updatedDate: '2024-03-18T01:52:02.050+00:00'
+    }
+  }
+]
