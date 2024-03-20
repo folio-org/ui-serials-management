0b00bcfa43346761d23ed50fd3b37e1607ad22a1
MonireRasouli <monireh.rasouli@k-int.co.uk>
2024-03-20 17:20:25 +0000

    test coverage for PatternTimePeriodForm

--- src/components/RulesetFormSections/PatternTimePeriodForm/PatternTimePeriodForm.test.js
+++ src/components/RulesetFormSections/PatternTimePeriodForm/PatternTimePeriodForm.test.js
@@ -1,35 +1,227 @@
+import { screen } from '@folio/jest-config-stripes/testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { renderWithIntl, TestForm } from '@folio/stripes-erm-testing';
+import {
+  renderWithIntl,
+  TestForm,
+  TextField,
+  Select,
+  Button,
+} from '@folio/stripes-erm-testing';
 
 import PatternTimePeriodForm from './PatternTimePeriodForm';
 import { translationsProperties } from '../../../../test/helpers';
 
+import mockRefdata from '../../../../test/resources/refdata';
+
+jest.mock('../../utils', () => ({
+  ...jest.requireActual('../../utils'),
+  useSerialsManagementRefdata: () => mockRefdata,
+}));
+
+const weeksProp = {
+  rulesetStatus: {
+    value: 'active',
+  },
+  recurrence: {
+    timeUnit: {
+      value: 'week',
+    },
+    period: '2',
+    issues: '-1',
+  },
+  patternType: 'week',
+};
+
+const monthsProp = {
+  rulesetStatus: {
+    value: 'active',
+  },
+  recurrence: {
+    timeUnit: {
+      value: 'month',
+    },
+    period: '2',
+    issues: '-1',
+  },
+  patternType: 'month',
+};
+
+const dayProps = {
+  rulesetStatus: {
+    value: 'active',
+  },
+  recurrence: {
+    timeUnit: {
+      value: 'day',
+    },
+    period: '1',
+    issues: '3',
+  },
+  patternType: 'day',
+};
+
+const yearsProp = {
+  rulesetStatus: {
+    value: 'active',
+  },
+  recurrence: {
+    timeUnit: {
+      value: 'year',
+    },
+    period: '2',
+    issues: '-1',
+  },
+  patternType: 'year',
+};
+
 const onSubmit = jest.fn();
 describe('PatternTimePeriodForm', () => {
-  let renderComponent;
-  beforeEach(() => {
-    renderComponent = renderWithIntl(
-      <MemoryRouter>
-        <TestForm onSubmit={onSubmit}>
-          <PatternTimePeriodForm />
-        </TestForm>
-      </MemoryRouter>,
-      translationsProperties
-    );
+  describe('PatternTimePeriodForm with selected week', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <TestForm onSubmit={onSubmit} initialValues={weeksProp}>
+            <PatternTimePeriodForm />
+          </TestForm>
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the expected Cycle length header', async () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Cycle length')).toBeInTheDocument();
+    });
+
+    test('renders the expected Time unit label', async () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Time unit')).toBeInTheDocument();
+    });
+
+    test('renders the expected number of weeks label', async () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Number of weeks')).toBeInTheDocument();
+    });
+
+    test('renders the expected number of published per cycle label', async () => {
+      const { getByText } = renderComponent;
+      expect(getByText('No. of issues published per cycle')).toBeInTheDocument();
+    });
+
+    it('renders expected Time unit selection', async () => {
+      await Select('Time unit*').exists();
+    });
+
+    it('renders expected time unit with selected option', async () => {
+      Select('Time unit').choose('Day');
+      Select('Time unit').choose('Week');
+      Select('Time unit').choose('Month');
+      Select('Time unit').choose('Year');
+    });
+
+    test('renders the Lable TextField', async () => {
+      await TextField({ id: 'number-of-time-unit' }).exists();
+    });
+
+    test('renders the Lable TextField', async () => {
+      await TextField('Number of weeks*').fillIn('1');
+    });
+
+    test('renders the Lable TextField', async () => {
+      await TextField({ id: 'number-of-issue-per-cycle' }).exists();
+    });
+
+    test('renders the Lable TextField', async () => {
+      await TextField('No. of issues published per cycle*').fillIn('1');
+    });
+
+    test('renders the submit button', async () => {
+      await Button('Submit').exists();
+    });
   });
 
-  test('renders the expected label', async () => {
-    const { getByText } = renderComponent;
-    expect(getByText('Cycle length')).toBeInTheDocument();
+  describe('PatternTimePeriodForm with month selected', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <TestForm onSubmit={onSubmit} initialValues={monthsProp}>
+            <PatternTimePeriodForm />
+          </TestForm>
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the expected number of months label', async () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Number of months')).toBeInTheDocument();
+    });
+
+    test('renders the Lable TextField', async () => {
+      await TextField('Number of months*').fillIn('2');
+    });
   });
 
-  test('renders the expected label', async () => {
-    const { getByText } = renderComponent;
-    expect(getByText('Time unit')).toBeInTheDocument();
+  describe('PatternTimePeriodForm with day selected', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <TestForm onSubmit={onSubmit} initialValues={dayProps}>
+            <PatternTimePeriodForm />
+          </TestForm>
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the expected number of days label', async () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Number of days')).toBeInTheDocument();
+    });
+
+    test('renders the Lable TextField', async () => {
+      await TextField('Number of days*').fillIn('2');
+    });
   });
 
-  test('renders the expected label', async () => {
-    const { getByText } = renderComponent;
-    expect(getByText('Number of time units')).toBeInTheDocument();
+  describe('PatternTimePeriodForm with year selected', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <TestForm onSubmit={onSubmit} initialValues={yearsProp}>
+            <PatternTimePeriodForm />
+          </TestForm>
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the expected number of years label', async () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Number of years')).toBeInTheDocument();
+    });
+
+    test('renders the Lable TextField', async () => {
+      await TextField('Number of years*').fillIn('1');
+    });
+
+    test('renders the correct heading', async () => {
+      const { getByRole } = renderComponent;
+      expect(getByRole('heading', { name: 'Cycle length' })).toBeInTheDocument();
+    });
+
+    test('renders the correct heading', async () => {
+      const { getByRole } = renderComponent;
+      expect(getByRole('combobox', { name: 'Time unit' })).toBeInTheDocument();
+    });
+
+    test('renders the save and close button', () => {
+      const { findAllByText } = renderComponent;
+      expect(findAllByText('button', { name: 'info' }));
+    });
   });
 });
