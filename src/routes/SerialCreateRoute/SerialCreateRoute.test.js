1ff5507dc9d03c7e95ed061badabb4b733437ac5
MonireRasouli <monireh.rasouli@k-int.co.uk>
2024-03-26 10:48:32 +0000

    additional tests

--- src/routes/SerialCreateRoute/SerialCreateRoute.test.js
+++ src/routes/SerialCreateRoute/SerialCreateRoute.test.js
@@ -1,44 +1,10 @@
 import { renderWithIntl } from '@folio/stripes-erm-testing';
 import { MemoryRouter } from 'react-router-dom';
-
 import SerialCreateRoute from './SerialCreateRoute';
 
 import { translationsProperties } from '../../../test/helpers';
 
 jest.mock('../../components/views/SerialForm', () => () => <div>SerialForm</div>);
-
-const history = {
-  length: 14,
-  action: 'PUSH',
-  location: {
-    pathname: '/serials-management/serials/create',
-    search: '',
-    hash: '',
-    key: 'pfrl8e',
-  },
-  createHref: 'ƒ createHref() {}',
-  push: 'ƒ push() {}',
-  replace: 'ƒ replace() {}',
-  go: 'ƒ go() {}',
-  goBack: 'ƒ goBack() {}',
-  goForward: 'ƒ goForward() {}',
-  block: 'ƒ block() {}',
-  listen: 'ƒ listen() {}',
-};
-
-const location = {
-  pathname: '/serials-management/serials/create',
-  search: '',
-  hash: '',
-  key: 'pfrl8e',
-};
-
-const match = {
-  path: '/serials-management/serials/create',
-  url: '/serials-management/serials/create',
-  isExact: true,
-  params: '{}',
-};
 
 let renderComponent;
 
@@ -46,7 +12,7 @@ describe('SerialCreateRoute', () => {
   beforeEach(() => {
     renderComponent = renderWithIntl(
       <MemoryRouter>
-        <SerialCreateRoute history={history} location={location} match={match} />
+        <SerialCreateRoute />
       </MemoryRouter>,
       translationsProperties
     );
