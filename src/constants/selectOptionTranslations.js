67f5109409d9c865a1e9e1990cfac67c48a8a42a
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2024-04-04 16:28:08 +0100

    fix: Select options translations (#225)
    * fix: Select options translations
    
    Updated select options constants file to ensure that field options are correctly translated
    
    * refactor: Publication pattern code smell
    
    Resolved major codee smell within publication pattern component

--- src/constants/selectOptionTranslations.js
+++ src/constants/selectOptionTranslations.js
@@ -1,32 +1,33 @@
-// TODO This needs to be handled by translations or some other system to allow for internationalisation
+// These constants reflect the refdatavalues:
+// 'Global.WeekdayFormat', 'Global.MonthDayFormat', 'Global.MonthFormat', 'Global.YearFormat, EnumerationNumericLevelTMRF.Format
 
 const CHRONOLOGY_WEEKDAY_FORMAT = [
-  { value: 'full_lower', label: 'Monday' },
-  { value: 'full_upper', label: 'MONDAY' },
-  { value: 'slice_lower', label: 'Mon' },
-  { value: 'slice_upper', label: 'MON' },
+  { value: 'full_lower', id: 'ui-serials-management.ruleset.chronology.weekday.fullLower' },
+  { value: 'full_upper', id: 'ui-serials-management.ruleset.chronology.weekday.fullUpper' },
+  { value: 'slice_lower', id: 'ui-serials-management.ruleset.chronology.weekday.sliceLower' },
+  { value: 'slice_upper', id: 'ui-serials-management.ruleset.chronology.weekday.sliceUpper' },
 ];
 
 const CHRONOLOGY_MONTH_DAY_FORMAT = [
-  { value: 'number', label: '3' },
-  { value: 'ordinal', label: '3rd' },
+  { value: 'number', id: 'ui-serials-management.ruleset.chronology.monthDay.number' },
+  { value: 'ordinal', id: 'ui-serials-management.ruleset.chronology.monthDay.ordinal' },
 ];
 
 const CHRONOLOGY_MONTH_FORMAT = [
-  { value: 'full', label: 'October' },
-  { value: 'number', label: '8' },
-  { value: 'slice', label: 'Oct' },
+  { value: 'full', id: 'ui-serials-management.ruleset.chronology.month.full' },
+  { value: 'number', id: 'ui-serials-management.ruleset.chronology.month.number' },
+  { value: 'slice', id: 'ui-serials-management.ruleset.chronology.month.slice' },
 ];
 
 const CHRONOLOGY_YEAR_FORMAT = [
-  { value: 'full', label: '2023' },
-  { value: 'slice', label: '23' },
+  { value: 'full', id: 'ui-serials-management.ruleset.chronology.year.full' },
+  { value: 'slice', id: 'ui-serials-management.ruleset.chronology.year.slice' },
 ];
 
 const ENUMERATION_NUMBER_FORMAT = [
-  { value: 'number', label: 'Number (1, 2, 3)' },
-  { value: 'ordinal', label: 'Ordinal (1st, 2nd, 3rd)' },
-  { value: 'roman', label: 'Roman numerals (I, II, III)' },
+  { value: 'number', id: 'ui-serials-management.ruleset.enumeration.numberFormat.number' },
+  { value: 'ordinal', id: 'ui-serials-management.ruleset.enumeration.numberFormat.ordinal' },
+  { value: 'roman', id: 'ui-serials-management.ruleset.enumeration.numberFormat.roman' },
 ];
 
 export {
