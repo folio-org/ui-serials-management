3e3e68013663a5764a19f393a6ae45949f36e862
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2024-09-04 17:18:25 +0200

    * extend test

--- src/components/PiecesPreviewModal/PiecesPreviewModal.test.js
+++ src/components/PiecesPreviewModal/PiecesPreviewModal.test.js
@@ -2,18 +2,17 @@ import { useState } from 'react';
 import { useMutation } from 'react-query';
 import {
   renderWithIntl,
-  Datepicker,
   Button,
+  Datepicker,
   TextField,
 } from '@folio/stripes-erm-testing';
 
 import { waitFor, screen } from '@folio/jest-config-stripes/testing-library/react';
-import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
 
 import PiecesPreviewModal from './PiecesPreviewModal';
 
 import { translationsProperties } from '../../../test/helpers';
-import { pieceSets, ruleset } from '../../../test/resources';
+import { pieceSets, ruleset, rulesetPreview } from '../../../test/resources';
 
 /* EXAMPLE Mocking useMutation to allow us to test the .then clause */
 const mockMutateAsync = jest.fn(() => Promise.resolve(true));
@@ -33,18 +32,17 @@ const TestComponent = () => {
   );
 };
 
-// const TestComponent2 = () => {
-//   // We need actual state in here for close test
-//   const [showModal, setShowModal] = useState(true);
+const TestComponent2 = () => {
+  const [showModal, setShowModal] = useState(true);
 
-//   return (
-//     <PiecesPreviewModal
-//       ruleset={ruleset}
-//       setShowModal={setShowModal}
-//       showModal={showModal}
-//     />
-//   );
-// };
+  return (
+    <PiecesPreviewModal
+      ruleset={rulesetPreview}
+      setShowModal={setShowModal}
+      showModal={showModal}
+    />
+  );
+};
 
 let renderComponent;
 describe('PiecesPreviewModal', () => {
@@ -113,22 +111,15 @@ describe('PiecesPreviewModal', () => {
     await Button({ id: 'close-button' }).has({ disabled: false });
   });
 
-  describe('PiecesPreviewModal Interactions', () => {
+  describe('PiecesPreviewModal overlapping piece set', () => {
     test('types a predicted piece date into Datepicker, see warning, generate button enabled, click it and see confirmation modal', async () => {
-      const { getByText } = renderComponent;
+      const { getByText, queryByText } = renderComponent;
       await waitFor(async () => {
-        // Datepicker({ id: 'ruleset-start-date' }).focus();
-        await Datepicker('Start date*').focus();
-        // await Datepicker('Start date*').fillIn('10/05/2024');
-        await Datepicker('Start date*').fillIn(pieceSets[0].pieces[0].date);
-        // await Datepicker('Start date*').fillIn('01/01/2025');
-        await Datepicker('Start date*').blur();
-        // Datepicker({ id: 'ruleset-start-date' }).blur();
-        // await Datepicker({ id: 'cc-start-date-0' }).has({ inputValue: '01/20/2021' });
+        await Datepicker({ id: 'ruleset-start-date' }).focus();
+        await Datepicker({ id: 'ruleset-start-date' }).fillIn('10/05/2024');
+        await Datepicker({ id: 'ruleset-start-date' }).blur();
       });
-      screen.debug();
-      await Datepicker('Start date*').has({ inputValue: '10/05/2024' });
-      // await Datepicker('Start date*').has({ inputValue: '01/01/2025' });
+      await Datepicker({ id: 'ruleset-start-date' }).has({ inputValue: '10/05/2024' });
 
       await waitFor(async () => expect(getByText(/Warning: A predicted piece set with the start date/i)).toBeInTheDocument());
       await Button({ id: 'generate-predicted-pieces-button' }).has({ disabled: false });
@@ -136,31 +127,84 @@ describe('PiecesPreviewModal', () => {
         await Button({ id: 'generate-predicted-pieces-button' }).click();
       });
       expect(getByText('Confirm generation of overlapping piece sets')).toBeInTheDocument();
-      screen.debug();
 
-      // await Button({ id: 'clickable-generate-confirmation-modal-cancel' }).has({ disabled: false });
       await Button('Cancel generation').has({ disabled: false });
       await Button({ id: 'clickable-generate-confirmation-modal-confirm' }).has({ disabled: false });
 
-      // Following not working atm
       await waitFor(async () => {
-        // await Button({ id: 'clickable-generate-confirmation-modal-cancel' }).click();
         await Button('Cancel generation').click();
       });
-      // expect(getByText('Confirm generation of overlapping piece sets')).not.toBeInTheDocument();
+      await waitFor(async () => {
+        expect(queryByText('Confirm generation of overlapping piece sets')).not.toBeInTheDocument();
+      });
     });
   });
 });
 
-// describe('PiecesPreviewModal w/o allowCreation', () => {
-//   beforeEach(() => {
-//     useMutation.mockReturnValue({ mutateAsync: mockMutateAsync });
+describe('PiecesPreviewModal generate', () => {
+  beforeEach(() => {
+    useMutation.mockReturnValue({ mutateAsync: mockMutateAsync });
 
-//     renderComponent = renderWithIntl(<TestComponent2 />, translationsProperties);
-//   });
+    renderComponent = renderWithIntl(<TestComponent />, translationsProperties);
+  });
 
-//   test('renders the expected modal header', async () => {
-//     const { getByText } = renderComponent;
-//     expect(getByText('Preview predicted pieces')).toBeInTheDocument();
-//   });
-// });
+  test('types a date into Datepicker, generate button enabled', async () => {
+    await waitFor(async () => {
+      Datepicker({ id: 'ruleset-start-date' }).focus();
+      await Datepicker({ id: 'ruleset-start-date' }).fillIn('01/01/2025');
+      await Datepicker({ id: 'ruleset-start-date' }).blur();
+    });
+    // screen.debug();
+    await Datepicker({ id: 'ruleset-start-date' }).has({ inputValue: '01/01/2025' });
+
+    await Button({ id: 'generate-predicted-pieces-button' }).has({ disabled: false });
+    await waitFor(async () => {
+      await Button({ id: 'generate-predicted-pieces-button' }).click();
+    });
+    screen.debug();
+
+    // Following not working atm
+    // await waitFor(async () => {
+    // expect(mockMutateAsync).toHaveBeenCalled();
+    // });
+  });
+});
+
+describe('PiecesPreviewModal w/o allowCreation', () => {
+  beforeEach(() => {
+    useMutation.mockReturnValue({ mutateAsync: mockMutateAsync });
+
+    renderComponent = renderWithIntl(<TestComponent2 />, translationsProperties);
+  });
+
+  test('useMutation has been called', () => {
+    expect(useMutation).toHaveBeenCalled();
+  });
+
+  test('renders the expected modal header', async () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Preview predicted pieces')).toBeInTheDocument();
+  });
+
+  test('renders the preview button', async () => {
+    await Button({ id: 'rulset-preview-button' }).has({ disabled: true });
+  });
+
+  test('types a date into Datepicker, preview button enabled, click it', async () => {
+    await waitFor(async () => {
+      await Datepicker({ id: 'ruleset-start-date' }).focus();
+      await Datepicker({ id: 'ruleset-start-date' }).fillIn('01/01/2025');
+      await Datepicker({ id: 'ruleset-start-date' }).blur();
+    });
+    await Datepicker({ id: 'ruleset-start-date' }).has({ inputValue: '01/01/2025' });
+
+    await Button({ id: 'rulset-preview-button' }).has({ disabled: false });
+    await waitFor(async () => {
+      await Button({ id: 'rulset-preview-button' }).click();
+    });
+
+    await waitFor(async () => {
+      expect(mockMutateAsync).toHaveBeenCalled();
+    });
+  });
+});
