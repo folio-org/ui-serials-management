f72c377a90dc93fbd8a28394236d6f2bff4c7f7b
Jack Golding <jack.golding@k-int.com>
2024-09-23 11:01:39 +0100

    feat: Deleted predicted piece set
    Added ability for user to delete predicted piece set given that they have the correct permission and no prior receiving pieces have been generated using the piece set

--- src/components/views/PieceSetView/PieceSetView.js
+++ src/components/views/PieceSetView/PieceSetView.js
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useQuery } from 'react-query';
-import { useOkapiKy } from '@folio/stripes/core';
-import { FormattedMessage } from 'react-intl';
-import { Pane, LoadingPane, Button, Icon } from '@folio/stripes/components';
+import { useQuery, useMutation } from 'react-query';
+import { useOkapiKy, useStripes } from '@folio/stripes/core';
+import { FormattedMessage, useIntl } from 'react-intl';
+import {
+  Pane,
+  LoadingPane,
+  Button,
+  Icon,
+  ConfirmationModal,
+} from '@folio/stripes/components';
 import { DEFAULT_VIEW_PANE_WIDTH } from '../../../constants/config';
 import { PieceSetInfo, PiecesList } from '../../PieceSetSections';
 
 import GenerateReceivingModal from '../../GenerateReceivingModal/GenerateReceivingModal';
-import { SERIAL_ENDPOINT } from '../../../constants/endpoints';
+import {
+  SERIAL_ENDPOINT,
+  PIECE_SET_ENDPOINT,
+} from '../../../constants/endpoints';
 
 const propTypes = {
   onClose: PropTypes.func.isRequired,
@@ -22,17 +31,32 @@ const PieceSetView = ({
   queryProps: { isLoading },
 }) => {
   const ky = useOkapiKy();
+  const stripes = useStripes();
+  const intl = useIntl();
+  const [showDeleteConfirmModal, setShowDeleteConfirmModal] = useState(false);
   const [showReceivingModal, setShowReceivingModal] = useState(false);
 
   const { data: serial, isLoading: serialLoading } = useQuery(
     [
       'ui-serials-management',
-      'GenerateReceivingModal',
+      'PieceSetView',
+      'generateReceving',
       pieceSet?.ruleset?.owner?.id,
     ],
     () => ky.get(SERIAL_ENDPOINT(pieceSet?.ruleset?.owner?.id)).json(),
     { enabled: !!pieceSet?.ruleset?.owner?.id }
   );
+
+  const { mutateAsync: deletePieceSet } = useMutation(
+    ['ui-serials-management', 'PieceSetView', 'deletePieceSet', pieceSet?.id],
+    () => {
+      ky.delete(PIECE_SET_ENDPOINT(pieceSet?.id));
+    }
+  );
+
+  const handleDelete = async () => {
+    await deletePieceSet(pieceSet?.id);
+  };
 
   const getHoldingIds = () => {
     if (serial?.orderLine?.remoteId_object?.locations?.[0]?.holdingId) {
@@ -59,10 +83,10 @@ const PieceSetView = ({
     const buttons = [];
     buttons.push(
       <Button
-        key="edit-serial-option"
+        key="generate-receiving-option"
         buttonStyle="dropdownItem"
         disabled={!serial?.orderLine?.remoteId_object || serialLoading}
-        id="clickable-dropdown-edit-serial"
+        id="clickable-dropdown-generate-receiving"
         onClick={() => setShowReceivingModal(true)}
       >
         <Icon icon="edit">
@@ -70,6 +94,21 @@ const PieceSetView = ({
         </Icon>
       </Button>
     );
+    if (stripes?.hasPerm('ui-serials-management.predictedpieces.edit')) {
+      buttons.push(
+        <Button
+          key="delete-piece-set-option"
+          buttonStyle="dropdownItem"
+          // disabled={}
+          id="clickable-dropdown-delete-piece-set"
+          onClick={() => setShowDeleteConfirmModal(true)}
+        >
+          <Icon icon="edit">
+            <FormattedMessage id="ui-serials-management.pieceSets.deletePredictedPieceSet" />
+          </Icon>
+        </Button>
+      );
+    }
     return buttons.length ? buttons : null;
   };
 
@@ -97,6 +136,20 @@ const PieceSetView = ({
         serial={serial}
         setShowModal={setShowReceivingModal}
         showModal={showReceivingModal}
+      />
+      <ConfirmationModal
+        cancelLabel={<FormattedMessage id="ui-serials-management.cancel" />}
+        confirmLabel={<FormattedMessage id="ui-serials-management.delete" />}
+        heading={intl.formatMessage({
+          id: 'ui-serials-management.pieceSets.deletePredictedPieceSet',
+        })}
+        id="delete-piece-set-confirmation"
+        message={
+          <FormattedMessage id="ui-serials-management.pieceSets.deletePredictedPieceSet.confirmMessage" />
+        }
+        onCancel={() => setShowDeleteConfirmModal(false)}
+        onConfirm={handleDelete}
+        open={showDeleteConfirmModal}
       />
     </>
   );
