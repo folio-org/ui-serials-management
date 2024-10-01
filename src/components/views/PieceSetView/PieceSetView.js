5cb3d965b3147f3392b503cad2e52905e3c2fe71
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2024-10-01 17:50:35 +0100

    feat: Deleted predicted piece set (#305)
    * feat: Deleted predicted piece set
    
    Added ability for user to delete predicted piece set given that they have the correct permission and no prior receiving pieces have been generated using the piece set
    
    * feat: Handle delete
    
    Added handle delete to aciton button
    
    * test: Pieceset view
    
    Tweaked action button tests
    
    * test: Test fix
    
    * revert: Action button tests
    
    * test: Actions buttons
    
    * test: Fixed resource test file
    
    Fixed array issue withintesting file
    
    * test: Fixed action menu button test
    
    * test: Loading pane test
    
    Added test for rendering loading pane
    
    * test: Render confirmation modal test
    
    Added test for rendering of confirmation modal
    
    * test: Closing confirmation modal test
    
    Added test for checking the closing functionality of the confirmation modal
    
    * test: Delete piece set test
    
    Added test deleting the predicted piece set
    
    * chore: Linting
    
    ---------
    
    Co-authored-by: EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>

--- src/components/views/PieceSetView/PieceSetView.js
+++ src/components/views/PieceSetView/PieceSetView.js
@@ -1,14 +1,25 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useQuery } from 'react-query';
-import { useOkapiKy } from '@folio/stripes/core';
-import { FormattedMessage } from 'react-intl';
-import { Pane, LoadingPane, Button, Icon } from '@folio/stripes/components';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { useOkapiKy, useStripes } from '@folio/stripes/core';
+import { FormattedMessage, useIntl } from 'react-intl';
+import {
+  Pane,
+  LoadingPane,
+  Button,
+  Icon,
+  ConfirmationModal,
+} from '@folio/stripes/components';
 import { DEFAULT_VIEW_PANE_WIDTH } from '../../../constants/config';
 import { PieceSetInfo, PiecesList } from '../../PieceSetSections';
 
 import GenerateReceivingModal from '../../GenerateReceivingModal/GenerateReceivingModal';
-import { SERIAL_ENDPOINT } from '../../../constants/endpoints';
+import {
+  SERIAL_ENDPOINT,
+  PIECE_SET_ENDPOINT,
+} from '../../../constants/endpoints';
+import { urls } from '../../utils';
 
 const propTypes = {
   onClose: PropTypes.func.isRequired,
@@ -22,17 +33,42 @@ const PieceSetView = ({
   queryProps: { isLoading },
 }) => {
   const ky = useOkapiKy();
+  const stripes = useStripes();
+  const intl = useIntl();
+  const history = useHistory();
+  const location = useLocation();
+  const queryClient = useQueryClient();
+
+  const [showDeleteConfirmModal, setShowDeleteConfirmModal] = useState(false);
   const [showReceivingModal, setShowReceivingModal] = useState(false);
 
   const { data: serial, isLoading: serialLoading } = useQuery(
     [
       'ui-serials-management',
-      'GenerateReceivingModal',
+      'PieceSetView',
+      'generateReceving',
       pieceSet?.ruleset?.owner?.id,
     ],
     () => ky.get(SERIAL_ENDPOINT(pieceSet?.ruleset?.owner?.id)).json(),
     { enabled: !!pieceSet?.ruleset?.owner?.id }
   );
+
+  const { mutateAsync: deletePieceSet } = useMutation(
+    ['ui-serials-management', 'PieceSetView', 'deletePieceSet', pieceSet?.id],
+    () => {
+      ky.delete(PIECE_SET_ENDPOINT(pieceSet?.id)).then(() => queryClient.invalidateQueries([
+        '@folio/serials-management',
+        'SASQ',
+        'piece-sets',
+        'viewAll'
+      ]));
+    }
+  );
+
+  const handleDelete = async () => {
+    await deletePieceSet(pieceSet?.id);
+    history.push(`${urls.pieceSets()}${location.search}`);
+  };
 
   const getHoldingIds = () => {
     if (serial?.orderLine?.remoteId_object?.locations?.[0]?.holdingId) {
@@ -57,12 +93,16 @@ const PieceSetView = ({
 
   const renderActionMenu = () => {
     const buttons = [];
+    // Check to see if pieceset contains pieces that have been generated in receiving
+    const hasReceiving = pieceSet?.pieces?.some(
+      (p) => p?.receivingPieces?.length >= 1
+    );
     buttons.push(
       <Button
-        key="edit-serial-option"
+        key="generate-receiving-option"
         buttonStyle="dropdownItem"
         disabled={!serial?.orderLine?.remoteId_object || serialLoading}
-        id="clickable-dropdown-edit-serial"
+        id="clickable-dropdown-generate-receiving"
         onClick={() => setShowReceivingModal(true)}
       >
         <Icon icon="edit">
@@ -70,6 +110,21 @@ const PieceSetView = ({
         </Icon>
       </Button>
     );
+    if (stripes?.hasPerm('ui-serials-management.predictedpieces.edit')) {
+      buttons.push(
+        <Button
+          key="delete-piece-set-option"
+          buttonStyle="dropdownItem"
+          disabled={hasReceiving}
+          id="clickable-dropdown-delete-piece-set"
+          onClick={() => setShowDeleteConfirmModal(true)}
+        >
+          <Icon icon="trash">
+            <FormattedMessage id="ui-serials-management.pieceSets.deletePredictedPieceSet" />
+          </Icon>
+        </Button>
+      );
+    }
     return buttons.length ? buttons : null;
   };
 
@@ -97,6 +152,20 @@ const PieceSetView = ({
         serial={serial}
         setShowModal={setShowReceivingModal}
         showModal={showReceivingModal}
+      />
+      <ConfirmationModal
+        cancelLabel={<FormattedMessage id="ui-serials-management.cancel" />}
+        confirmLabel={<FormattedMessage id="ui-serials-management.delete" />}
+        heading={intl.formatMessage({
+          id: 'ui-serials-management.pieceSets.deletePredictedPieceSet',
+        })}
+        id="delete-piece-set-confirmation"
+        message={
+          <FormattedMessage id="ui-serials-management.pieceSets.deletePredictedPieceSet.confirmMessage" />
+        }
+        onCancel={() => setShowDeleteConfirmModal(false)}
+        onConfirm={handleDelete}
+        open={showDeleteConfirmModal}
       />
     </>
   );
