5ce7549b0914a60319a99daf2a2b8a39b36ee961
Jack Golding <jack.golding@k-int.com>
2024-10-02 17:00:23 +0100

    chore: Removed pieces resource
    Removed pieces test resource, was unused so probably wasnt removed when it was deprecated

--- test/resources/index.js
+++ test/resources/index.js
@@ -5,7 +5,6 @@ export { default as pieceSet } from './pieceSet';
 export { default as ruleset } from './ruleset';
 export { default as locations } from './locations';
 export { default as holdings } from './holdings';
-export { default as pieceSets } from './pieceSets';
 export { default as resource } from './resource';
 export { default as orderLine } from './orderLine';
 export { default as locales } from './locales';
