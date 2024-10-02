8d1a82804817bfc3978d8cc03e131b5baccf712c
Jack Golding <jack.golding@k-int.com>
2024-10-02 17:04:27 +0100

    test: Removed "resource" test resource
    Removed "resource" test resource, it was a pieceSet object and we already have one of those, updated tests to reflect this change

--- test/resources/index.js
+++ test/resources/index.js
@@ -5,6 +5,5 @@ export { default as pieceSet } from './pieceSet';
 export { default as ruleset } from './ruleset';
 export { default as locations } from './locations';
 export { default as holdings } from './holdings';
-export { default as resource } from './resource';
 export { default as orderLine } from './orderLine';
 export { default as locales } from './locales';
