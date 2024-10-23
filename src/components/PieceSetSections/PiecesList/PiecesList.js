2c29282b743d7caff8435285ee931de9aa955fbc
Jack Golding <jack.golding@k-int.com>
2024-10-23 22:19:27 +0100

    fix: Sorted pieces
    Moved piece sorting to pieceset view, may need to be used outside of this component in the future, also resolved issue with the internalPieceClasses constants

--- src/components/PieceSetSections/PiecesList/PiecesList.js
+++ src/components/PieceSetSections/PiecesList/PiecesList.js
@@ -3,8 +3,6 @@ import { FormattedMessage } from 'react-intl';
 
 import { Row, Col, MultiColumnList } from '@folio/stripes/components';
 
-import { INTERNAL_COMBINATION_PIECE } from '../../../constants/internalPieceClasses';
-
 import PiecePublicationDate from '../../PiecePublicationDate';
 
 const propTypes = {
@@ -28,31 +26,6 @@ const PiecesList = ({ pieceSet, id }) => {
     receivingPieces: (e) => e?.receivingPieces?.length,
   };
 
-  // This is a bit annoying with regard to the sorting that needs to be done
-  // Grails doesnt like attempting to sort uniderectional one to many relationships
-  const sortedPieces = pieceSet?.pieces
-  // Itereate through the pieces and find combination pieces
-    ?.map((p) => {
-      return p?.class === INTERNAL_COMBINATION_PIECE
-        ? {
-          ...p,
-          // Sort combination piece's recurrence pieces by date
-          recurrencePieces: p?.recurrencePieces?.sort((a, b) => (a?.date < b?.date ? -1 : 1)),
-        }
-        : p;
-    })
-    .sort((a, b) => {
-      // Sort all pieces by date or by the recurrence pieces of a combination piece
-      return (a?.class === INTERNAL_COMBINATION_PIECE
-        ? a?.recurrencePieces?.[0]?.date
-        : a?.date) <
-        (b?.class === INTERNAL_COMBINATION_PIECE
-          ? b?.recurrencePieces?.[0]?.date
-          : b?.date)
-        ? -1
-        : 1;
-    });
-
   return (
     <div id={id}>
       <Row start="xs">
@@ -75,7 +48,7 @@ const PiecesList = ({ pieceSet, id }) => {
             columnWidths={{
               publicationDate: { min: 100, max: 165 },
             }}
-            contentData={sortedPieces}
+            contentData={pieceSet?.pieces}
             formatter={formatter}
             interactive={false}
             visibleColumns={[
