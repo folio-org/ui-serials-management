39b899a3b90988f55b16c791b142630c50d73ec5
Jack Golding <jack.golding@k-int.com>
2024-11-01 20:06:30 +0000

    refactor: Chronology refdata values
    Added findRefdataValue to chronology resources

--- test/resources/rulesetResources/chronologyRules.js
+++ test/resources/rulesetResources/chronologyRules.js
@@ -1,41 +1,25 @@
+import { findRefdataValue } from '../utils';
+
 export const chronologyDate = {
   id: '5000849c-7745-4ec6-a583-04a5b31e209d',
   index: 0,
-  templateMetadataRuleType: {
-    id: '2c91809f92e7cf460192e7d912770054',
-    value: 'chronology',
-    label: 'Chronology',
-  },
+  templateMetadataRuleType: findRefdataValue(
+    'TemplateMetadataRule.TemplateMetadataRuleType',
+    'chronology'
+  ),
   ruleType: {
     id: 'a14ac8e2-d6ae-4887-aeb0-9343927e4dee',
     ruleLocale: 'en',
-    templateMetadataRuleFormat: {
-      id: '2c91809f92e7cf460192e7d913b70062',
-      value: 'chronology_date',
-      label: 'Date',
-    },
+    templateMetadataRuleFormat: findRefdataValue(
+      'ChronologyTemplateMetadataRule.TemplateMetadataRuleFormat',
+      'chronology_date'
+    ),
     ruleFormat: {
       id: 'd85e7ab6-824e-43bc-88af-0152b8877d9f',
-      yearFormat: {
-        id: '2c91809f92e7cf460192e7d90fe50016',
-        value: 'full',
-        label: 'Full',
-      },
-      monthFormat: {
-        id: '2c91809f92e7cf460192e7d90ff30019',
-        value: 'full',
-        label: 'Full',
-      },
-      weekdayFormat: {
-        id: '2c91809f92e7cf460192e7d91010001f',
-        value: 'full_lower',
-        label: 'Full Lower',
-      },
-      monthDayFormat: {
-        id: '2c91809f92e7cf460192e7d910220023',
-        value: 'number',
-        label: 'Number',
-      },
+      yearFormat: findRefdataValue('Global.YearFormat', 'full'),
+      monthFormat: findRefdataValue('Global.MonthFormat', 'full'),
+      weekdayFormat: findRefdataValue('Global.WeekdayFormat', 'full_lower'),
+      monthDayFormat: findRefdataValue('Global.MonthDayFormat', 'number'),
     },
   },
 };
@@ -43,31 +27,21 @@ export const chronologyDate = {
 export const chronologyMonth = {
   id: '920623d1-de6a-4916-957b-a6a00a8b84ef',
   index: 1,
-  templateMetadataRuleType: {
-    id: '2c91809f92e7cf460192e7d912770054',
-    value: 'chronology',
-    label: 'Chronology',
-  },
+  templateMetadataRuleType: findRefdataValue(
+    'TemplateMetadataRule.TemplateMetadataRuleType',
+    'chronology'
+  ),
   ruleType: {
     id: 'f2f7ab49-bed0-49e2-ad14-e3d2d56e5bd6',
     ruleLocale: 'en',
-    templateMetadataRuleFormat: {
-      id: '2c91809f92e7cf460192e7d913b90063',
-      value: 'chronology_month',
-      label: 'Month',
-    },
+    templateMetadataRuleFormat: findRefdataValue(
+      'ChronologyTemplateMetadataRule.TemplateMetadataRuleFormat',
+      'chronology_month'
+    ),
     ruleFormat: {
       id: 'a252d854-5f73-4dcc-bf6e-c184543c1234',
-      yearFormat: {
-        id: '2c91809f92e7cf460192e7d90fe50016',
-        value: 'full',
-        label: 'Full',
-      },
-      monthFormat: {
-        id: '2c91809f92e7cf460192e7d90ff30019',
-        value: 'full',
-        label: 'Full',
-      },
+      yearFormat: findRefdataValue('Global.YearFormat', 'full'),
+      monthFormat: findRefdataValue('Global.MonthFormat', 'full'),
     },
   },
 };
@@ -75,26 +49,20 @@ export const chronologyMonth = {
 export const chronologyYear = {
   id: '6c14f945-ce66-4c30-aed7-fbb5a771a203',
   index: 2,
-  templateMetadataRuleType: {
-    id: '2c91809f92e7cf460192e7d912770054',
-    value: 'chronology',
-    label: 'Chronology',
-  },
+  templateMetadataRuleType: findRefdataValue(
+    'TemplateMetadataRule.TemplateMetadataRuleType',
+    'chronology'
+  ),
   ruleType: {
     id: 'f663fcd3-5cb5-4878-b9fe-0f5eee24aed8',
     ruleLocale: 'en',
-    templateMetadataRuleFormat: {
-      id: '2c91809f92e7cf460192e7d913bb0064',
-      value: 'chronology_year',
-      label: 'Year',
-    },
+    templateMetadataRuleFormat: findRefdataValue(
+      'ChronologyTemplateMetadataRule.TemplateMetadataRuleFormat',
+      'chronology_year'
+    ),
     ruleFormat: {
       id: 'be3fca50-21c3-438c-8dc0-fbfff4ea2c64',
-      yearFormat: {
-        id: '2c91809f92e7cf460192e7d90fe50016',
-        value: 'full',
-        label: 'Full',
-      },
+      yearFormat: findRefdataValue('Global.YearFormat', 'full'),
     },
   },
 };
