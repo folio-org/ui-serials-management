e2c90866fe22b14f0a965ed6755ac8279610a10c
Jack Golding <jack.golding@k-int.com>
2024-11-01 17:34:22 +0000

    refactor: Omission rules refdata values
    Added find refdata value function to omission rules resource

--- test/resources/rulesetResources/omissionsRules.js
+++ test/resources/rulesetResources/omissionsRules.js
@@ -1,89 +1,43 @@
+import { findRefdataValue } from '../utils/findRefdataValue';
+
 export const dayWeekMonth = {
   id: '8b7e9470-9524-4a16-b2ae-99709865ad21',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49bf0025',
-    value: 'day',
-    label: 'Day',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49e8002e',
-    value: 'day_week_month',
-    label: 'Day Week Month',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'day'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'day_week_month'),
   pattern: {
     id: '73370b00-848e-4fa4-95bd-a947068253c5',
     week: 1,
-    month: {
-      id: '2c91809f92e151a60192e15b48d40001',
-      value: 'january',
-      label: 'January',
-    },
-    weekday: {
-      id: '2c91809f92e151a60192e15b495e000e',
-      value: 'monday',
-      label: 'Monday',
-    },
+    month: findRefdataValue('Global.Month', 'january'),
+    weekday: findRefdataValue('Global.Weekday', 'monday'),
   },
 };
 
 export const dayMonth = {
   id: 'b22878a2-c8c2-4f41-bd09-c683a8d52e8e',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49bf0025',
-    value: 'day',
-    label: 'Day',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49d8002b',
-    value: 'day_month',
-    label: 'Day Month',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'day'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'day_month'),
   pattern: {
     id: 'f0235750-45af-47bd-bbca-b8d9b0b65f8a',
-    month: {
-      id: '2c91809f92e151a60192e15b48d40001',
-      value: 'january',
-      label: 'January',
-    },
+    month: findRefdataValue('Global.Month', 'january'),
     day: 1,
   },
 };
 
 export const dayWeek = {
   id: '68c46cdd-b394-4541-a971-1b850b27b49c',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49bf0025',
-    value: 'day',
-    label: 'Day',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49dd002c',
-    value: 'day_week',
-    label: 'Day Week',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'day'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'day_week'),
   pattern: {
     id: 'a09a09fa-78cb-4070-acd4-10753d447183',
     week: 7,
-    weekday: {
-      id: '2c91809f92e151a60192e15b495e000e',
-      value: 'monday',
-      label: 'Monday',
-    },
+    weekday: findRefdataValue('Global.Weekday', 'monday'),
   },
 };
 
 export const day = {
   id: '969f96f2-a317-4f48-b6d6-19f7a6169e7c',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49bf0025',
-    value: 'day',
-    label: 'Day',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49d3002a',
-    value: 'day',
-    label: 'Day',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'day'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'day'),
   pattern: {
     id: '8135965a-ea9f-4d23-8941-af385f943860',
     day: 1,
@@ -92,61 +46,29 @@ export const day = {
 
 export const dayWeekday = {
   id: 'e60d38ce-158b-449f-82be-cb6f6d6e6bb9',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49bf0025',
-    value: 'day',
-    label: 'Day',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49e3002d',
-    value: 'day_weekday',
-    label: 'Day Weekday',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'day'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'day_weekday'),
   pattern: {
     id: '94222dc9-1494-4692-b933-0b94a9330e6f',
-    weekday: {
-      id: '2c91809f92e151a60192e15b495e000e',
-      value: 'monday',
-      label: 'Monday',
-    },
+    weekday: findRefdataValue('Global.Weekday', 'monday'),
   },
 };
 
 export const weekMonth = {
   id: 'aefdb819-3743-4061-a1cd-3c224969ebbf',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49c40026',
-    value: 'week',
-    label: 'Week',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49f40030',
-    value: 'week_month',
-    label: 'Week Month',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'week'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'week_month'),
   pattern: {
     id: '4d2d86ea-7f98-4607-b758-774cfdc97743',
     week: 1,
-    month: {
-      id: '2c91809f92e151a60192e15b48d40001',
-      value: 'january',
-      label: 'January',
-    },
+    month: findRefdataValue('Global.Month', 'january'),
   },
 };
 
 export const week = {
   id: '2993d8d2-3b1e-4670-a75d-caae9660be66',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49c40026',
-    value: 'week',
-    label: 'Week',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49ef002f',
-    value: 'week',
-    label: 'Week',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'week'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'week'),
   pattern: {
     id: '1b33c4af-91fd-4169-91c3-ebaeee94a27e',
     isRange: false,
@@ -156,16 +78,8 @@ export const week = {
 
 export const weekRange = {
   id: 'd8978fa6-be59-4f80-ab3d-f82b3e90218d',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49c40026',
-    value: 'week',
-    label: 'Week',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49ef002f',
-    value: 'week',
-    label: 'Week',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'week'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'week'),
   pattern: {
     id: '1798f825-d03f-4a7d-847b-1c60f2030a50',
     isRange: true,
@@ -176,16 +90,8 @@ export const weekRange = {
 
 export const month = {
   id: '4016dd97-021a-4bc1-9151-912be4d68631',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49c90027',
-    value: 'month',
-    label: 'Month',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49fb0031',
-    value: 'month',
-    label: 'Month',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'month'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'month'),
   pattern: {
     id: 'b10130aa-4a7f-4ae6-9b95-f4cd86cb3e7d',
     monthFrom: {
@@ -199,16 +105,8 @@ export const month = {
 
 export const monthRange = {
   id: '3931aef3-fed2-45ad-916c-876419346013',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49c90027',
-    value: 'month',
-    label: 'Month',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49fb0031',
-    value: 'month',
-    label: 'Month',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'month'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'month'),
   pattern: {
     id: '096e4a40-cbd5-425b-8604-96a60bf88ed2',
     monthTo: {
@@ -227,16 +125,8 @@ export const monthRange = {
 
 export const issue = {
   id: 'aade24d9-8f0c-41e9-a70e-4d789849fc08',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49ce0028',
-    value: 'issue',
-    label: 'Issue',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b49ff0032',
-    value: 'issue',
-    label: 'Issue',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'issue'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'issue'),
   pattern: {
     id: '16bed840-7d67-4ccd-acd7-5d6a59a63e2a',
     issue: 1,
@@ -245,16 +135,8 @@ export const issue = {
 
 export const issueWeek = {
   id: '2d54c307-5883-468c-a9cb-9e13973b229d',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49ce0028',
-    value: 'issue',
-    label: 'Issue',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b4a040033',
-    value: 'issue_week',
-    label: 'Issue Week',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'issue'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'issue_week'),
   pattern: {
     id: '74d79bd1-dede-4b9f-a623-27b95cd46450',
     week: 10,
@@ -264,47 +146,23 @@ export const issueWeek = {
 
 export const issueWeekMonth = {
   id: '549ade86-f26c-4cbe-b6ed-787c20c2cf99',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49ce0028',
-    value: 'issue',
-    label: 'Issue',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b4a090034',
-    value: 'issue_week_month',
-    label: 'Issue Week Month',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'issue'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'issue_week_month'),
   pattern: {
     id: 'e2e95e68-97be-4f80-8752-422a1fbc0550',
     week: 1,
-    month: {
-      id: '2c91809f92e151a60192e15b48d40001',
-      value: 'january',
-      label: 'January',
-    },
+    month: findRefdataValue('Global.Month', 'january'),
     issue: 1,
   },
 };
 
 export const issueMonth = {
   id: 'e724322d-9d74-43e2-8b11-cc849b6d8043',
-  timeUnit: {
-    id: '2c91809f92e151a60192e15b49ce0028',
-    value: 'issue',
-    label: 'Issue',
-  },
-  patternType: {
-    id: '2c91809f92e151a60192e15b4a0d0035',
-    value: 'issue_month',
-    label: 'Issue Month',
-  },
+  timeUnit: findRefdataValue('OmissionRule.TimeUnits', 'issue'),
+  patternType: findRefdataValue('OmissionRule.PatternType', 'issue_month'),
   pattern: {
     id: '7e97faaa-5b69-48d8-a322-072448b60e47',
-    month: {
-      id: '2c91809f92e151a60192e15b48d40001',
-      value: 'january',
-      label: 'January',
-    },
+    month: findRefdataValue('Global.Month', 'january'),
     issue: 1,
   },
 };
