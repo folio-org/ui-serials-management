0a270ce8a32920b225a625622446ef3111f7dc53
Jack Golding <jack.golding@k-int.com>
2024-11-01 19:55:35 +0000

    chore: Updated locales
    Refetched locales with keyLocales param to reflect what is used within the UI

--- test/resources/locales.js
+++ test/resources/locales.js
@@ -1,4 +1,3 @@
-// TODO Needs refetching with key locales
 export default [
   {
     label: '',
@@ -9,187 +8,847 @@ export default [
     value: 'af',
   },
   {
-    label: 'Afrikaans (Latin, South Africa)',
-    value: 'af_ZA_#Latn',
-  },
-  {
-    label: 'Afrikaans (Namibia)',
-    value: 'af_NA',
-  },
-  {
-    label: 'Afrikaans (South Africa)',
-    value: 'af_ZA',
-  },
-  {
     label: 'Aghem',
     value: 'agq',
-  },
-  {
-    label: 'Aghem (Cameroon)',
-    value: 'agq_CM',
-  },
-  {
-    label: 'Aghem (Latin, Cameroon)',
-    value: 'agq_CM_#Latn',
   },
   {
     label: 'Akan',
     value: 'ak',
   },
   {
-    label: 'Akan (Ghana)',
-    value: 'ak_GH',
-  },
-  {
-    label: 'Akan (Latin, Ghana)',
-    value: 'ak_GH_#Latn',
-  },
-  {
     label: 'Albanian',
     value: 'sq',
-  },
-  {
-    label: 'Albanian (Albania)',
-    value: 'sq_AL',
-  },
-  {
-    label: 'Albanian (Kosovo)',
-    value: 'sq_XK',
-  },
-  {
-    label: 'Albanian (Latin, Albania)',
-    value: 'sq_AL_#Latn',
-  },
-  {
-    label: 'Albanian (North Macedonia)',
-    value: 'sq_MK',
   },
   {
     label: 'Amharic',
     value: 'am',
   },
   {
-    label: 'Amharic (Ethiopia)',
-    value: 'am_ET',
-  },
-  {
-    label: 'Amharic (Ethiopic, Ethiopia)',
-    value: 'am_ET_#Ethi',
-  },
-  {
     label: 'Arabic',
     value: 'ar',
   },
   {
-    label: 'Arabic (Algeria)',
-    value: 'ar_DZ',
+    label: 'Armenian',
+    value: 'hy',
   },
   {
-    label: 'Arabic (Arabic, Egypt)',
-    value: 'ar_EG_#Arab',
+    label: 'Assamese',
+    value: 'as',
   },
   {
-    label: 'Arabic (Bahrain)',
-    value: 'ar_BH',
+    label: 'Asturian',
+    value: 'ast',
   },
   {
-    label: 'Arabic (Chad)',
-    value: 'ar_TD',
+    label: 'Asu',
+    value: 'asa',
   },
   {
-    label: 'Arabic (Comoros)',
-    value: 'ar_KM',
+    label: 'Azerbaijani',
+    value: 'az',
   },
   {
-    label: 'Arabic (Djibouti)',
-    value: 'ar_DJ',
+    label: 'Bafia',
+    value: 'ksf',
   },
   {
-    label: 'Arabic (Egypt)',
-    value: 'ar_EG',
+    label: 'Bambara',
+    value: 'bm',
   },
   {
-    label: 'Arabic (Eritrea)',
-    value: 'ar_ER',
+    label: 'Bangla',
+    value: 'bn',
   },
   {
-    label: 'Arabic (Iraq)',
-    value: 'ar_IQ',
+    label: 'Basaa',
+    value: 'bas',
   },
   {
-    label: 'Arabic (Israel)',
-    value: 'ar_IL',
+    label: 'Basque',
+    value: 'eu',
   },
   {
-    label: 'Arabic (Jordan)',
-    value: 'ar_JO',
+    label: 'Belarusian',
+    value: 'be',
   },
   {
-    label: 'Arabic (Kuwait)',
-    value: 'ar_KW',
+    label: 'Bemba',
+    value: 'bem',
   },
   {
-    label: 'Arabic (Lebanon)',
-    value: 'ar_LB',
+    label: 'Bena',
+    value: 'bez',
   },
   {
-    label: 'Arabic (Libya)',
-    value: 'ar_LY',
+    label: 'Bodo',
+    value: 'brx',
   },
   {
-    label: 'Arabic (Mauritania)',
-    value: 'ar_MR',
+    label: 'Bosnian',
+    value: 'bs',
   },
   {
-    label: 'Arabic (Morocco)',
-    value: 'ar_MA',
+    label: 'Breton',
+    value: 'br',
   },
   {
-    label: 'Arabic (Oman)',
-    value: 'ar_OM',
+    label: 'Bulgarian',
+    value: 'bg',
   },
   {
-    label: 'Arabic (Palestinian Territories)',
-    value: 'ar_PS',
+    label: 'Burmese',
+    value: 'my',
   },
   {
-    label: 'Arabic (Qatar)',
-    value: 'ar_QA',
+    label: 'Cantonese',
+    value: 'yue',
   },
   {
-    label: 'Colognian (Latin, Germany)',
-    value: 'ksh_DE_#Latn',
+    label: 'Catalan',
+    value: 'ca',
+  },
+  {
+    label: 'Cebuano',
+    value: 'ceb',
+  },
+  {
+    label: 'Central Atlas Tamazight',
+    value: 'tzm',
+  },
+  {
+    label: 'Central Kurdish',
+    value: 'ckb',
+  },
+  {
+    label: 'Chakma',
+    value: 'ccp',
+  },
+  {
+    label: 'Chechen',
+    value: 'ce',
+  },
+  {
+    label: 'Cherokee',
+    value: 'chr',
+  },
+  {
+    label: 'Chiga',
+    value: 'cgg',
+  },
+  {
+    label: 'Chinese',
+    value: 'zh',
+  },
+  {
+    label: 'Colognian',
+    value: 'ksh',
   },
   {
     label: 'Cornish',
     value: 'kw',
   },
   {
-    label: 'Cornish (Latin, United Kingdom)',
-    value: 'kw_GB_#Latn',
-  },
-  {
-    label: 'Cornish (United Kingdom)',
-    value: 'kw_GB',
-  },
-  {
     label: 'Croatian',
     value: 'hr',
   },
   {
-    label: 'Croatian (Bosnia & Herzegovina)',
-    value: 'hr_BA',
-  },
-  {
-    label: 'Croatian (Croatia)',
-    value: 'hr_HR',
-  },
-  {
-    label: 'Croatian (Latin, Croatia)',
-    value: 'hr_HR_#Latn',
-  },
-  {
     label: 'Czech',
     value: 'cs',
+  },
+  {
+    label: 'Danish',
+    value: 'da',
+  },
+  {
+    label: 'Dogri',
+    value: 'doi',
+  },
+  {
+    label: 'Duala',
+    value: 'dua',
+  },
+  {
+    label: 'Dutch',
+    value: 'nl',
+  },
+  {
+    label: 'Dzongkha',
+    value: 'dz',
+  },
+  {
+    label: 'Embu',
+    value: 'ebu',
+  },
+  {
+    label: 'English',
+    value: 'en',
+  },
+  {
+    label: 'Esperanto',
+    value: 'eo',
+  },
+  {
+    label: 'Estonian',
+    value: 'et',
+  },
+  {
+    label: 'Ewe',
+    value: 'ee',
+  },
+  {
+    label: 'Ewondo',
+    value: 'ewo',
+  },
+  {
+    label: 'Faroese',
+    value: 'fo',
+  },
+  {
+    label: 'Filipino',
+    value: 'fil',
+  },
+  {
+    label: 'Finnish',
+    value: 'fi',
+  },
+  {
+    label: 'French',
+    value: 'fr',
+  },
+  {
+    label: 'Friulian',
+    value: 'fur',
+  },
+  {
+    label: 'Fulah',
+    value: 'ff',
+  },
+  {
+    label: 'Galician',
+    value: 'gl',
+  },
+  {
+    label: 'Ganda',
+    value: 'lg',
+  },
+  {
+    label: 'Georgian',
+    value: 'ka',
+  },
+  {
+    label: 'German',
+    value: 'de',
+  },
+  {
+    label: 'Greek',
+    value: 'el',
+  },
+  {
+    label: 'Gujarati',
+    value: 'gu',
+  },
+  {
+    label: 'Gusii',
+    value: 'guz',
+  },
+  {
+    label: 'Hausa',
+    value: 'ha',
+  },
+  {
+    label: 'Hawaiian',
+    value: 'haw',
+  },
+  {
+    label: 'Hebrew',
+    value: 'he',
+  },
+  {
+    label: 'Hindi',
+    value: 'hi',
+  },
+  {
+    label: 'Hungarian',
+    value: 'hu',
+  },
+  {
+    label: 'Icelandic',
+    value: 'is',
+  },
+  {
+    label: 'Igbo',
+    value: 'ig',
+  },
+  {
+    label: 'Inari Sami',
+    value: 'smn',
+  },
+  {
+    label: 'Indonesian',
+    value: 'id',
+  },
+  {
+    label: 'Interlingua',
+    value: 'ia',
+  },
+  {
+    label: 'Irish',
+    value: 'ga',
+  },
+  {
+    label: 'Italian',
+    value: 'it',
+  },
+  {
+    label: 'Japanese',
+    value: 'ja',
+  },
+  {
+    label: 'Javanese',
+    value: 'jv',
+  },
+  {
+    label: 'Jola-Fonyi',
+    value: 'dyo',
+  },
+  {
+    label: 'Kabuverdianu',
+    value: 'kea',
+  },
+  {
+    label: 'Kabyle',
+    value: 'kab',
+  },
+  {
+    label: 'Kako',
+    value: 'kkj',
+  },
+  {
+    label: 'Kalaallisut',
+    value: 'kl',
+  },
+  {
+    label: 'Kalenjin',
+    value: 'kln',
+  },
+  {
+    label: 'Kamba',
+    value: 'kam',
+  },
+  {
+    label: 'Kannada',
+    value: 'kn',
+  },
+  {
+    label: 'Kashmiri',
+    value: 'ks',
+  },
+  {
+    label: 'Kazakh',
+    value: 'kk',
+  },
+  {
+    label: 'Khmer',
+    value: 'km',
+  },
+  {
+    label: 'Kikuyu',
+    value: 'ki',
+  },
+  {
+    label: 'Kinyarwanda',
+    value: 'rw',
+  },
+  {
+    label: 'Konkani',
+    value: 'kok',
+  },
+  {
+    label: 'Korean',
+    value: 'ko',
+  },
+  {
+    label: 'Koyra Chiini',
+    value: 'khq',
+  },
+  {
+    label: 'Koyraboro Senni',
+    value: 'ses',
+  },
+  {
+    label: 'Kurdish',
+    value: 'ku',
+  },
+  {
+    label: 'Kwasio',
+    value: 'nmg',
+  },
+  {
+    label: 'Kyrgyz',
+    value: 'ky',
+  },
+  {
+    label: 'Lakota',
+    value: 'lkt',
+  },
+  {
+    label: 'Langi',
+    value: 'lag',
+  },
+  {
+    label: 'Lao',
+    value: 'lo',
+  },
+  {
+    label: 'Latvian',
+    value: 'lv',
+  },
+  {
+    label: 'Lingala',
+    value: 'ln',
+  },
+  {
+    label: 'Lithuanian',
+    value: 'lt',
+  },
+  {
+    label: 'Low German',
+    value: 'nds',
+  },
+  {
+    label: 'Lower Sorbian',
+    value: 'dsb',
+  },
+  {
+    label: 'Luba-Katanga',
+    value: 'lu',
+  },
+  {
+    label: 'Luo',
+    value: 'luo',
+  },
+  {
+    label: 'Luxembourgish',
+    value: 'lb',
+  },
+  {
+    label: 'Luyia',
+    value: 'luy',
+  },
+  {
+    label: 'Macedonian',
+    value: 'mk',
+  },
+  {
+    label: 'Machame',
+    value: 'jmc',
+  },
+  {
+    label: 'Maithili',
+    value: 'mai',
+  },
+  {
+    label: 'Makhuwa-Meetto',
+    value: 'mgh',
+  },
+  {
+    label: 'Makonde',
+    value: 'kde',
+  },
+  {
+    label: 'Malagasy',
+    value: 'mg',
+  },
+  {
+    label: 'Malay',
+    value: 'ms',
+  },
+  {
+    label: 'Malayalam',
+    value: 'ml',
+  },
+  {
+    label: 'Maltese',
+    value: 'mt',
+  },
+  {
+    label: 'Manipuri',
+    value: 'mni',
+  },
+  {
+    label: 'Manx',
+    value: 'gv',
+  },
+  {
+    label: 'Maori',
+    value: 'mi',
+  },
+  {
+    label: 'Marathi',
+    value: 'mr',
+  },
+  {
+    label: 'Masai',
+    value: 'mas',
+  },
+  {
+    label: 'Mazanderani',
+    value: 'mzn',
+  },
+  {
+    label: 'Meru',
+    value: 'mer',
+  },
+  {
+    label: 'Metaʼ',
+    value: 'mgo',
+  },
+  {
+    label: 'Mongolian',
+    value: 'mn',
+  },
+  {
+    label: 'Morisyen',
+    value: 'mfe',
+  },
+  {
+    label: 'Mundang',
+    value: 'mua',
+  },
+  {
+    label: 'Nama',
+    value: 'naq',
+  },
+  {
+    label: 'Nepali',
+    value: 'ne',
+  },
+  {
+    label: 'Ngiemboon',
+    value: 'nnh',
+  },
+  {
+    label: 'Ngomba',
+    value: 'jgo',
+  },
+  {
+    label: 'Nigerian Pidgin',
+    value: 'pcm',
+  },
+  {
+    label: 'North Ndebele',
+    value: 'nd',
+  },
+  {
+    label: 'Northern Luri',
+    value: 'lrc',
+  },
+  {
+    label: 'Northern Sami',
+    value: 'se',
+  },
+  {
+    label: 'Norwegian',
+    value: 'no',
+  },
+  {
+    label: 'Norwegian Bokmål',
+    value: 'nb',
+  },
+  {
+    label: 'Norwegian Nynorsk',
+    value: 'nn',
+  },
+  {
+    label: 'Nuer',
+    value: 'nus',
+  },
+  {
+    label: 'Nyankole',
+    value: 'nyn',
+  },
+  {
+    label: 'Odia',
+    value: 'or',
+  },
+  {
+    label: 'Oromo',
+    value: 'om',
+  },
+  {
+    label: 'Ossetic',
+    value: 'os',
+  },
+  {
+    label: 'Pashto',
+    value: 'ps',
+  },
+  {
+    label: 'Persian',
+    value: 'fa',
+  },
+  {
+    label: 'Polish',
+    value: 'pl',
+  },
+  {
+    label: 'Portuguese',
+    value: 'pt',
+  },
+  {
+    label: 'Punjabi',
+    value: 'pa',
+  },
+  {
+    label: 'Quechua',
+    value: 'qu',
+  },
+  {
+    label: 'Romanian',
+    value: 'ro',
+  },
+  {
+    label: 'Romansh',
+    value: 'rm',
+  },
+  {
+    label: 'Rombo',
+    value: 'rof',
+  },
+  {
+    label: 'Rundi',
+    value: 'rn',
+  },
+  {
+    label: 'Russian',
+    value: 'ru',
+  },
+  {
+    label: 'Rwa',
+    value: 'rwk',
+  },
+  {
+    label: 'Sakha',
+    value: 'sah',
+  },
+  {
+    label: 'Samburu',
+    value: 'saq',
+  },
+  {
+    label: 'Sango',
+    value: 'sg',
+  },
+  {
+    label: 'Sangu',
+    value: 'sbp',
+  },
+  {
+    label: 'Sanskrit',
+    value: 'sa',
+  },
+  {
+    label: 'Santali',
+    value: 'sat',
+  },
+  {
+    label: 'Scottish Gaelic',
+    value: 'gd',
+  },
+  {
+    label: 'Sena',
+    value: 'seh',
+  },
+  {
+    label: 'Serbian',
+    value: 'sr',
+  },
+  {
+    label: 'Shambala',
+    value: 'ksb',
+  },
+  {
+    label: 'Shona',
+    value: 'sn',
+  },
+  {
+    label: 'Sichuan Yi',
+    value: 'ii',
+  },
+  {
+    label: 'Sindhi',
+    value: 'sd',
+  },
+  {
+    label: 'Sinhala',
+    value: 'si',
+  },
+  {
+    label: 'Slovak',
+    value: 'sk',
+  },
+  {
+    label: 'Slovenian',
+    value: 'sl',
+  },
+  {
+    label: 'Soga',
+    value: 'xog',
+  },
+  {
+    label: 'Somali',
+    value: 'so',
+  },
+  {
+    label: 'Spanish',
+    value: 'es',
+  },
+  {
+    label: 'Standard Moroccan Tamazight',
+    value: 'zgh',
+  },
+  {
+    label: 'Sundanese',
+    value: 'su',
+  },
+  {
+    label: 'Swahili',
+    value: 'sw',
+  },
+  {
+    label: 'Swedish',
+    value: 'sv',
+  },
+  {
+    label: 'Swiss German',
+    value: 'gsw',
+  },
+  {
+    label: 'Tachelhit',
+    value: 'shi',
+  },
+  {
+    label: 'Taita',
+    value: 'dav',
+  },
+  {
+    label: 'Tajik',
+    value: 'tg',
+  },
+  {
+    label: 'Tamil',
+    value: 'ta',
+  },
+  {
+    label: 'Tasawaq',
+    value: 'twq',
+  },
+  {
+    label: 'Tatar',
+    value: 'tt',
+  },
+  {
+    label: 'Telugu',
+    value: 'te',
+  },
+  {
+    label: 'Teso',
+    value: 'teo',
+  },
+  {
+    label: 'Thai',
+    value: 'th',
+  },
+  {
+    label: 'Tibetan',
+    value: 'bo',
+  },
+  {
+    label: 'Tigrinya',
+    value: 'ti',
+  },
+  {
+    label: 'Tongan',
+    value: 'to',
+  },
+  {
+    label: 'Turkish',
+    value: 'tr',
+  },
+  {
+    label: 'Turkmen',
+    value: 'tk',
+  },
+  {
+    label: 'Ukrainian',
+    value: 'uk',
+  },
+  {
+    label: 'Upper Sorbian',
+    value: 'hsb',
+  },
+  {
+    label: 'Urdu',
+    value: 'ur',
+  },
+  {
+    label: 'Uyghur',
+    value: 'ug',
+  },
+  {
+    label: 'Uzbek',
+    value: 'uz',
+  },
+  {
+    label: 'Vai',
+    value: 'vai',
+  },
+  {
+    label: 'Vietnamese',
+    value: 'vi',
+  },
+  {
+    label: 'Vunjo',
+    value: 'vun',
+  },
+  {
+    label: 'Walser',
+    value: 'wae',
+  },
+  {
+    label: 'Welsh',
+    value: 'cy',
+  },
+  {
+    label: 'Western Frisian',
+    value: 'fy',
+  },
+  {
+    label: 'Wolof',
+    value: 'wo',
+  },
+  {
+    label: 'Xhosa',
+    value: 'xh',
+  },
+  {
+    label: 'Yangben',
+    value: 'yav',
+  },
+  {
+    label: 'Yiddish',
+    value: 'yi',
+  },
+  {
+    label: 'Yoruba',
+    value: 'yo',
+  },
+  {
+    label: 'Zarma',
+    value: 'dje',
+  },
+  {
+    label: 'Zulu',
+    value: 'zu',
   },
 ];
