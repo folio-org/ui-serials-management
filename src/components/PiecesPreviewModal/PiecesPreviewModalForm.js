050ca50c1eeea130b22c905bd5298f815b338fd1
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2024-12-04 17:55:38 +0100

    UISER-179 Support option to generate multiple years/cycles when generating predicted pieces
    * add Number of of years / cycles field to PiecesPreviewModal form
    * add calculate allowed cycles function
    * use existing validators
    * add InfoPopover
    * extend validateWithinRange validator to handle optional custom message

--- src/components/PiecesPreviewModal/PiecesPreviewModalForm.js
+++ src/components/PiecesPreviewModal/PiecesPreviewModalForm.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Field, useForm, useFormState } from 'react-final-form';
 import { FormattedMessage, useIntl } from 'react-intl';
@@ -6,6 +7,7 @@ import {
   Datepicker,
   Row,
   Col,
+  InfoPopover,
   Label,
   TextField,
   Layout,
@@ -14,12 +16,14 @@ import {
   MessageBanner,
 } from '@folio/stripes/components';
 
+import { NumberField } from '@k-int/stripes-kint-components';
+
 import {
   composeValidators,
   requiredValidator,
 } from '@folio/stripes-erm-components';
 
-import { validateWithinRange } from '../utils';
+import { validateNotNegative, validateWithinRange } from '../utils';
 
 import css from './PiecesPreviewModal.css';
 
@@ -40,6 +44,34 @@ const PiecesPreviewModalForm = ({
   const { values } = useFormState();
   const { change } = useForm();
   const intl = useIntl();
+
+  useEffect(() => {
+    change('numberOfCycles', 1);
+  }, [change]);
+
+  const TIME_UNIT_PER_YEAR = {
+    day: { timeUnitsPerYear: 365.2425 },
+    week: { timeUnitsPerYear: 52.1775 },
+    month: { timeUnitsPerYear: 12 },
+  };
+
+  const MAX_PIECES = 366;
+
+  const calculateAllowedCycles = () => {
+    const { issues, period, timeUnit } = ruleset?.recurrence ?? {};
+
+    if (timeUnit?.value === 'year') {
+      return Math.floor((MAX_PIECES / issues));
+    } else {
+      const timeUnitsPerYear = TIME_UNIT_PER_YEAR[timeUnit?.value]?.timeUnitsPerYear;
+      return Math.floor(MAX_PIECES / ((timeUnitsPerYear / period) * issues));
+    }
+  };
+
+  const customValidationMessage = <FormattedMessage
+    id="ui-serials-management.validate.allowedCycles"
+    values={{ maxValue: calculateAllowedCycles() }}
+  />;
 
   const getAdjustedStartDate = (date) => {
     const adjustedStartDate = new Date(date);
@@ -141,7 +173,7 @@ const PiecesPreviewModalForm = ({
         {ruleset?.templateConfig?.rules?.map((e, i) => {
           if (
             e?.ruleType?.templateMetadataRuleFormat?.value ===
-              'enumeration_numeric' ||
+            'enumeration_numeric' ||
             e?.ruleType?.templateMetadataRuleFormat === 'enumeration_numeric'
           ) {
             // Required so that sonarcloud doesnt flag use of index within key prop
@@ -192,8 +224,35 @@ const PiecesPreviewModalForm = ({
             validate={requiredValidator}
           />
         </Col>
+        <Col xs={4}>
+          <Field
+            component={NumberField}
+            id="number-of-cycles"
+            label={
+              <>
+                <FormattedMessage id="ui-serials-management.ruleset.numberOfCycles" />
+                <InfoPopover
+                  content={
+                    <FormattedMessage
+                      id="ui-serials-management.numberOfCycles.infoPopover"
+                    />
+                  }
+                />
+              </>
+            }
+            name="numberOfCycles"
+            required
+            validate={composeValidators(
+              requiredValidator,
+              validateNotNegative,
+              validateWithinRange(1, calculateAllowedCycles(), customValidationMessage)
+            )}
+          />
+        </Col>
+      </Row>
+      <Row>
         {allowCreation && (
-          <Col xs={8}>
+          <Col xs={12}>
             <Field
               component={TextArea}
               label={
@@ -206,7 +265,7 @@ const PiecesPreviewModalForm = ({
       </Row>
       {!!ruleset?.templateConfig?.rules?.some(
         (e) => e?.ruleType?.templateMetadataRuleFormat?.value ===
-            'enumeration_numeric' ||
+          'enumeration_numeric' ||
           e?.ruleType?.templateMetadataRuleFormat === 'enumeration_numeric'
       ) && renderTemplateStartingValues()}
       {existingPieceSets?.some((ps) => ps?.startDate === values?.startDate) && (
