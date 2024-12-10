ad2d5f24c234da2935af1e63cbfb7df89ce06b68
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2024-12-10 14:55:55 +0000

    refactor: Generate receiving modal (#330)
    * refactor: Generate receiving modal
    
    Refactored generate eceiving modal to seperate out the info and form sections into their own components
    
    * build: Stripes-acq-components dependency
    
    Added strip[es-acq-components dev and peer dependencies
    
    * feat: useCentralOrderSettings hook
    
    WIP
    
    * feat: Tenants ids
    
    Added hook for fetching tenant Ids WIP
    
    * refactor: Generate Receiving and tests
    
    Refactored the generate receiving modal an child components futher, additionally created tests for each of the additional componentns
    
    * revert: Stripes-acq-components dependency
    
    Reverted change that added stripes acq dev and peer deps
    
    * test: fixed tests
    
    * fix: Combination display summary
    
    Fixed issue in which combination pieces were not being passed a correct display summary
    
    * chore: Linting
    
    * test: Test adjustments
    
    Refacctoring some of the components tests to pull in line with new centralised test resources
    
    * chore: Resource update
    
    * test: Combination piece set test
    
    Added a test case for a combination piece set
    
    * test: Tweaks
    
    * test: GenerateRecevingModalForm
    
    Updated test file to cover more test cases and utilise centralised test resources
    
    * test: No locations test case
    
    Added final test case for GenerateReceivingModalForm
    
    * fix: Array conditionals
    
    Added stricter conditions when checking for array length
    
    * refactor: Handling of receiving modal state
    
    * refactor: Filestructure
    
    Moved around files within GenerateReceivingModal for future work on API layer

---
+++ test/resources/rulesets/combinationRuleset.js
@@ -0,0 +1,29 @@
+import { chronologyDate } from '../rulesetResources/chronologyRules';
+import { issue } from '../rulesetResources/combinationRules';
+import { monthDate } from '../rulesetResources/recurrences';
+import { findRefdataValue } from '../utils';
+
+const combinationRuleset = {
+  id: '1dc064f2-2409-4cdb-9502-f1e186a9a5c7',
+  combination: {
+    id: '02143bbc-f68c-4156-9ec8-c440c5d39157',
+    rules: [issue],
+  },
+  dateCreated: '2024-11-05T14:08:44Z',
+  recurrence: monthDate,
+  lastUpdated: '2024-11-05T14:08:44Z',
+  rulesetStatus: findRefdataValue('SerialRuleset.RulesetStatus', 'draft'),
+  owner: {
+    id: '9be5cebf-c676-4430-9439-4f47973d8a47',
+  },
+  rulesetNumber: '5',
+  templateConfig: {
+    id: '28cb5cab-ce30-4b48-8d1b-2c7f86b2232e',
+    rules: [chronologyDate],
+    templateString:
+      '{{chronology1.weekday}} {{chronology1.monthDay}} {{chronology1.month}} {{chronology1.year}}',
+  },
+  description: 'Month date with combination on first and second issue',
+};
+
+export default combinationRuleset;
