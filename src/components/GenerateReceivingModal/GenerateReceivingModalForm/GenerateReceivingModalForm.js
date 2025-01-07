aded6cde94618698b2031e3fbef51917f78fb78f
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2025-01-07 13:57:04 +0000

    UISER-161: Enable support for serials to create receiving pieces in ECS environments with central ordering (#343)
    * build: Added stripes-acq-components dependency
    
    * feat: Implemented new hooks
    
    Implemented hooks from stripes-acq-components and removed duplicate hooks, namely useLocations and useHoldings
    
    * feat: Implemented handling of ecs environment handling
    
    Added use of consortium related hooks, the current functionality should now work within ecs environments
    
    * feat: Added use of consortium tenants
    
    Added consortium tenants hook and conditional use of callNumber
    
    * feat: Initial work on formatting locations/holdings
    
    Initial work on figuring out how to filter locations/holdings
    
    * feat: More affiliations work
    
    * feat: More work on handling holdings/locations
    
    Kind of a mind-bender but got something at least working
    
    * feat: First working version
    
    Finally managed to wrangle the locations/holdings into a place where it should work as expected
    
    * fix: Generate receiving submit handler
    
    Added receivingTenantId to the submit handler so that this is also passed when present
    
    * refactor: Locations/Holding/Tenants handling
    
    Refactored the locations/holding/tenants filtering and handling functions
    
    * fix: Affiliation onChange
    
    Added on change fucntion for the affiliation field
    
    * docs: Code comments
    
    Added code comments to clear up confusion
    
    * chore: Linting
    
    * refactor: Form layout
    
    Reordered elements of the form to better accomodate new affiliation field
    
    * feat: Dynamic locations hook
    
    Added locations hook that uses locations/consortium endpoint based on whether ecs settings is enabled
    
    * feat: useLocations/HoldingsQuery
    
    Implemented stripes-acq-compoennts hooks that do what was previously done by our own bespoke hook
    
    * fix: Final tweaks
    
    Fixed passing of consortium params to stripes-acq-hooks

--- src/components/GenerateReceivingModal/GenerateReceivingModalForm/GenerateReceivingModalForm.js
+++ src/components/GenerateReceivingModal/GenerateReceivingModalForm/GenerateReceivingModalForm.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import { Field } from 'react-final-form';
+import { useMemo } from 'react';
+import { Field, useForm, useFormState } from 'react-final-form';
 import { FormattedMessage } from 'react-intl';
 
 import { requiredValidator } from '@folio/stripes-erm-components';
@@ -11,45 +12,87 @@ import {
   InfoPopover,
   Select,
   Checkbox,
-  Label,
 } from '@folio/stripes/components';
 
 const propTypes = {
   orderLine: PropTypes.object,
   locations: PropTypes.arrayOf(PropTypes.object),
   holdings: PropTypes.arrayOf(PropTypes.object),
+  tenants: PropTypes.arrayOf(PropTypes.object),
 };
 
 const GenerateReceivingModalForm = ({
   orderLine,
   holdings = [],
   locations = [],
+  tenants = [],
 }) => {
-  const formatDataOptions = () => {
+  const { values } = useFormState();
+  const { change, batch } = useForm();
+
+  // Figuring out what locations/holdings go into the location/holding field
+  const locationsDataOptions = useMemo(() => {
     // If there are locations associated with a POL
-    if (orderLine?.remoteId_object?.locations?.length > 0) {
-      // And none of these are holdings
-      if (!holdings?.length && locations?.length > 0) {
-        // Data options should be an array of matched locations
-        return orderLine?.remoteId_object?.locations?.map((e) => {
-          const location = locations?.find((l) => e?.locationId === l?.id);
-          return { label: location?.name, value: location?.id };
-        });
-        // If both holdings and locations exist associated with the POL
-        // Data options should be the location, concatenated with the holding call number
-      } else if (locations?.length > 0 && holdings?.length > 0) {
-        return holdings?.map((h) => {
-          const holdingLocation = locations.find(
-            (l) => h?.permanentLocationId === l?.id
-          );
-          return {
-            label: `${holdingLocation?.name} > ${h?.callNumber}`,
-            value: h?.id,
-          };
-        });
-      }
+    // And none of these are holdings
+    if (!holdings?.length && locations?.length > 0) {
+      // Data options should be an array of matched locations
+      return (
+        locations
+          // if there are tenants, assume this is an ECS environment and also filter for matching tenantIds from the receivingTenantId field
+          ?.filter((l) => (tenants?.length > 0
+            ? l?.tenantId === values?.receivingTenantId
+            : true))
+          // Format into select field array format
+          ?.map((fl) => ({ label: fl?.name, value: fl?.id }))
+      );
+
+      // If both holdings and locations exist associated with the POL
+      // Data options should be the location, concatenated with the holding call number
+    } else if (locations?.length > 0 && holdings?.length > 0) {
+      return (
+        holdings
+          // if there are tenants, assume this is an ECS environment and also filter for matching tenantIds from the receivingTenantId field
+          ?.filter((h) => (tenants?.length > 0
+            ? h?.tenantId === values?.receivingTenantId
+            : true))
+          // Format into select field array format, also grabbing the location name from holdings associated location
+          ?.map((fh) => {
+            const holdingLocation = locations.find(
+              (l) => fh?.permanentLocationId === l?.id
+            );
+            return {
+              label: fh?.callNumber
+                ? `${holdingLocation?.name} > ${fh?.callNumber}`
+                : `${holdingLocation?.name}`,
+              value: fh?.id,
+            };
+          })
+      );
     }
     return [];
+  }, [holdings, locations, tenants?.length, values?.receivingTenantId]);
+
+  const affiliationDataOptions = useMemo(() => {
+    return tenants?.map((t) => ({
+      label: t?.code,
+      value: t?.id,
+    }));
+  }, [tenants]);
+
+  const affiliationOnChange = (e) => {
+    if (holdings?.length > 0) {
+      // Could do this onChange as we normally do but testing this
+      // Is there a reason we never really use batch for multiple change calls?
+      batch(() => {
+        change('receivingTenantId', e?.target?.value);
+        change('holdingId', undefined);
+      });
+    } else {
+      batch(() => {
+        change('receivingTenantId', e?.target?.value);
+        change('locationId', undefined);
+      });
+    }
   };
 
   return (
@@ -74,8 +117,6 @@ const GenerateReceivingModalForm = ({
             validate={requiredValidator}
           />
         </Col>
-      </Row>
-      <Row>
         <Col xs={6}>
           <Field
             component={Select}
@@ -87,53 +128,109 @@ const GenerateReceivingModalForm = ({
             name="format"
           />
         </Col>
-        <Col xs={3}>
-          <Label>
-            <FormattedMessage id="ui-serials-management.pieceSets.supplement" />
-            <InfoPopover
-              content={
-                <FormattedMessage id="ui-serials-management.pieceSets.supplementPopover" />
-              }
-              id="supplement-tooltip"
-            />
-          </Label>
-          <FormattedMessage id="ui-serials-management.pieceSets.supplement">
-            {([ariaLabel]) => (
-              <Field
-                aria-label={ariaLabel}
-                component={Checkbox}
-                name="supplement"
-                type="checkbox"
-              />
-            )}
-          </FormattedMessage>
+      </Row>
+      <Row>
+        <Col md={3} xs={6}>
+          <Field
+            component={Checkbox}
+            fullWidth
+            label={
+              <>
+                <FormattedMessage id="ui-serials-management.pieceSets.supplement" />
+                <InfoPopover
+                  content={
+                    <FormattedMessage id="ui-serials-management.pieceSets.supplementPopover" />
+                  }
+                  id="supplement-tooltip"
+                />
+              </>
+            }
+            name="supplement"
+            type="checkbox"
+            vertical
+          />
         </Col>
         {orderLine?.remoteId_object?.physical?.createInventory ===
           'Instance, Holding, Item' && (
-          <Col xs={3}>
-            <Label>
-              <FormattedMessage id="ui-serials-management.pieceSets.createItem" />
-            </Label>
-            <FormattedMessage id="ui-serials-management.pieceSets.createItem">
-              {([ariaLabel]) => (
-                <Field
-                  aria-label={ariaLabel}
-                  component={Checkbox}
-                  name="createItem"
-                  type="checkbox"
-                />
-              )}
-            </FormattedMessage>
+          <Col md={3} xs={6}>
+            <Field
+              component={Checkbox}
+              fullWidth
+              label={
+                <FormattedMessage id="ui-serials-management.pieceSets.createItem" />
+              }
+              name="createItem"
+              type="checkbox"
+              vertical
+            />
           </Col>
+        )}
+        {!!holdings?.length && (
+          <>
+            <Col md={3} xs={6}>
+              <Field
+                component={Checkbox}
+                fullWidth
+                label={
+                  <>
+                    <FormattedMessage id="ui-serials-management.pieceSets.displayInHolding" />
+                    <InfoPopover
+                      content={
+                        <FormattedMessage id="ui-serials-management.pieceSets.displayInHoldingPopover" />
+                      }
+                      id="display-on-holding-tooltip"
+                    />
+                  </>
+                }
+                name="displayOnHolding"
+                type="checkbox"
+                vertical
+              />
+            </Col>
+            <Col md={3} xs={6}>
+              <Field
+                component={Checkbox}
+                fullWidth
+                label={
+                  <FormattedMessage id="ui-serials-management.pieceSets.displayToPublic" />
+                }
+                name="displayToPublic"
+                type="checkbox"
+                vertical
+              />
+            </Col>
+          </>
         )}
       </Row>
       <Row>
+        {!!tenants?.length && (
+          <Col xs={6}>
+            <Field
+              component={Select}
+              dataOptions={[
+                { label: '', value: '' },
+                ...affiliationDataOptions,
+              ]}
+              disabled={orderLine?.remoteId_object?.locations?.length === 1}
+              label={
+                <FormattedMessage id="ui-serials-management.pieceSets.affiliation" />
+              }
+              name="receivingTenantId"
+              onChange={(e) => affiliationOnChange(e)}
+              required
+              validate={requiredValidator}
+            />
+          </Col>
+        )}
         {!!orderLine?.remoteId_object?.locations?.length && (
           <Col xs={6}>
             <Field
               component={Select}
-              dataOptions={[{ label: '', value: '' }, ...formatDataOptions()]}
-              disabled={orderLine?.remoteId_object?.locations?.length === 1}
+              dataOptions={[{ label: '', value: '' }, ...locationsDataOptions]}
+              disabled={
+                orderLine?.remoteId_object?.locations?.length === 1 ||
+                (tenants?.length > 0 && !values?.receivingTenantId)
+              }
               label={
                 holdings?.length ? (
                   <FormattedMessage id="ui-serials-management.pieceSets.holding" />
@@ -146,46 +243,6 @@ const GenerateReceivingModalForm = ({
               validate={requiredValidator}
             />
           </Col>
-        )}
-        {!!holdings?.length && (
-          <>
-            <Col xs={3}>
-              <Label>
-                <FormattedMessage id="ui-serials-management.pieceSets.displayInHolding" />
-                <InfoPopover
-                  content={
-                    <FormattedMessage id="ui-serials-management.pieceSets.displayInHoldingPopover" />
-                  }
-                  id="display-on-holding-tooltip"
-                />
-              </Label>
-              <FormattedMessage id="ui-serials-management.pieceSets.displayInHolding">
-                {([ariaLabel]) => (
-                  <Field
-                    aria-label={ariaLabel}
-                    component={Checkbox}
-                    name="displayOnHolding"
-                    type="checkbox"
-                  />
-                )}
-              </FormattedMessage>
-            </Col>
-            <Col xs={3}>
-              <Label>
-                <FormattedMessage id="ui-serials-management.pieceSets.displayToPublic" />
-              </Label>
-              <FormattedMessage id="ui-serials-management.pieceSets.displayToPublic">
-                {([ariaLabel]) => (
-                  <Field
-                    aria-label={ariaLabel}
-                    component={Checkbox}
-                    name="displayToPublic"
-                    type="checkbox"
-                  />
-                )}
-              </FormattedMessage>
-            </Col>
-          </>
         )}
       </Row>
     </>
