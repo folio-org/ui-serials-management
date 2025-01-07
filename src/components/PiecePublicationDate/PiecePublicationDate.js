e70e3618543dccf3049a8531b51d1c3cc1357bb8
Jack Golding <jack.golding@k-int.com>
2025-01-07 15:04:40 +0000

    fix: Combination pieces do not get allocated a date
    * fix: Initial combination piece sorting work
    
    Added initial work for sorted combination piece's recurrence pieces (bit confusing)
    
    * refactor: New generate fetch handling
    
    Updated some functions to handle new generate predicted pieces fetch (Object) whilst still supporting older version (Array), also updated some variable names for easier parsing
    
    * revert: Array/Object endpoint handling
    
    Reverted work for generated piece set handling or new/deprectaed endpoint responses, this is in a new ticket so should be part of a new PR
    
    * fix: Piece list sort
    
    Fixed the piece list so that now the pieces are correctly sorted regardless of class, additionally all combination pieces recurrence pieces are correctly sorted
    
    * fix: Sorted pieces
    
    Moved piece sorting to pieceset view, may need to be used outside of this component in the future, also resolved issue with the internalPieceClasses constants
    
    * refactor: Implement internal piece classes constants
    
    Added internal piece class constants to areas where omissionOrigins or combinationOrigins were used previously
    
    * fix: First/last piece information
    
    First/last piece information within the genrate receiving modal should now correctly handle combination pieces
    
    * fix: Predicted pieces information labelling
    
    Fixed an issue in which predicted pieces first/last date were not displaying correctly
    
    * refactor: Piece publication date
    
    As we've implemented the internal piece classes, seemed like a good time to refactor the piece publication date to use them aswell
    
    * test: Fixed tests
    
    Due to updated backend logic, alot of the tests needed to be updated

--- src/components/PiecePublicationDate/PiecePublicationDate.js
+++ src/components/PiecePublicationDate/PiecePublicationDate.js
@@ -2,34 +2,44 @@ import PropTypes from 'prop-types';
 import { FormattedMessage, FormattedDate } from 'react-intl';
 
 import { InfoBox } from '@folio/stripes-erm-components';
+import {
+  INTERNAL_COMBINATION_PIECE,
+  INTERNAL_OMISSION_PIECE,
+  INTERNAL_RECURRENCE_PIECE,
+} from '../../constants/internalPieceClasses';
 
 const propTypes = {
   piece: PropTypes.object,
 };
 
 const PiecePublicationDate = ({ piece }) => {
-  if (piece?.recurrencePieces) {
-    return (
-      <>
-        <FormattedDate value={piece?.recurrencePieces[0].date} />
-        <br />
-        <FormattedMessage
-          id="ui-serials-management.pieceSets.combinedPieces"
-          values={{ length: piece?.recurrencePieces.length }}
-        />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <FormattedDate value={piece?.date} />
-        {piece?.omissionOrigins && (
-          <InfoBox type="success">
-            <FormattedMessage id="ui-serials-management.pieceSets.omitted" />
-          </InfoBox>
-        )}
-      </>
-    );
+  switch (piece?.class) {
+    case INTERNAL_RECURRENCE_PIECE:
+      return <FormattedDate value={piece?.date} />;
+    case INTERNAL_OMISSION_PIECE:
+      return (
+        <>
+          <FormattedDate value={piece?.date} />
+          {piece?.class === INTERNAL_OMISSION_PIECE && (
+            <InfoBox type="success">
+              <FormattedMessage id="ui-serials-management.pieceSets.omitted" />
+            </InfoBox>
+          )}
+        </>
+      );
+    case INTERNAL_COMBINATION_PIECE:
+      return (
+        <>
+          <FormattedDate value={piece?.recurrencePieces[0].date} />
+          <br />
+          <FormattedMessage
+            id="ui-serials-management.pieceSets.combinedPieces"
+            values={{ length: piece?.recurrencePieces.length }}
+          />
+        </>
+      );
+    default:
+      return null;
   }
 };
 
