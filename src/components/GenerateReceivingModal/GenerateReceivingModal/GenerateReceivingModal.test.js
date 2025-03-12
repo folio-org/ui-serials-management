35c7644dacf59dfac5e8706bf40c84cdc9f99406
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-03-12 18:53:38 +0000

    test: Added proof of concept to up coverage on GenerateReceivingPiecesModal

--- src/components/GenerateReceivingModal/GenerateReceivingModal/GenerateReceivingModal.test.js
+++ src/components/GenerateReceivingModal/GenerateReceivingModal/GenerateReceivingModal.test.js
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import { useMutation } from 'react-query';
+import omit from 'lodash/omit';
 
-import { renderWithIntl, Button, Modal } from '@folio/stripes-erm-testing';
+import { renderWithIntl, Button, Modal, Select, TextField } from '@folio/stripes-erm-testing';
 
+// EXAMPLE reading the output from mockKy
+import { mockKy } from '@folio/stripes/core';
+
+import { waitFor, screen } from '@folio/jest-config-stripes/testing-library/react';
 import GenerateReceivingModal from './GenerateReceivingModal';
 
 import { translationsProperties } from '../../../../test/helpers';
@@ -19,38 +24,9 @@ jest.mock('../GenerateReceivingModalInfo', () => () => (
 
 jest.mock('@folio/stripes-acq-components', () => ({
   ...jest.requireActual('@folio/stripes-acq-components'),
-  useInstanceHoldings: () => ({ isLoading: false, data: mockHoldings }),
-  useLocations: () => ({ isLoading: false, data: mockLocations }),
+  useInstanceHoldingsQuery: () => ({ isLoading: false, holdings: mockHoldings }),
+  useLocationsQuery: () => ({ isLoading: false, locations: mockLocations }),
 }));
-
-/* EXAMPLE Mocking useMutation to allow us to test the .then clause */
-// Setting up jest fn here to test paramters passed in by component
-const mockPreview = jest.fn(() => Promise.resolve(true));
-const mockGenerate = jest.fn(() => Promise.resolve(true));
-jest.mock('react-query', () => {
-  const { mockReactQuery } = jest.requireActual('@folio/stripes-erm-testing');
-
-  return {
-    ...jest.requireActual('react-query'),
-    ...mockReactQuery,
-    useMutation: jest.fn((key, func) => ({
-      mutateAsync: (...incomingParams) => {
-        // Actually call function coming from component
-        // This assumes that ky has been mocked, which it should have been by __mocks__ stripes-core.
-
-        // If this function was async, we might need to do something different.
-        // As it is, it's a synchronous call to ky which returns a promise we then chain on.
-        func();
-
-        // Ensure we return the promise resolve from above, so that any _subsequent_ .then calls can flow
-        if (key.includes('submitReceivingPiece')) {
-          return mockPreview(...incomingParams);
-        }
-        return mockGenerate(...incomingParams);
-      },
-    })),
-  };
-});
 
 const TestComponent = () => {
   // We need actual state in here for close test
@@ -92,9 +68,78 @@ describe('GenerateReceivingModal', () => {
   //   expect(getByText('GenerateReceivingModalForm')).toBeInTheDocument();
   // });
 
-  test('renders the Generate receiving pieces button', async () => {
-    await Button('Generate receiving pieces').has({
-      disabled: true,
+  test('renders the Generate receiving pieces button (disabled)', async () => {
+    await waitFor(async () => {
+      await Button('Generate receiving pieces').has({
+        disabled: true,
+      });
+    });
+  });
+
+  describe('filling out generate modal form', () => {
+    beforeEach(async () => {
+      // Fill out fake value to enable button
+      await waitFor(async () => {
+        await TextField({ id: 'interval-field' }).fillIn('3');
+        await Select('Location*').chooseAndBlur('Annex');
+      });
+    });
+
+    test('renders the filled value as expected', async () => {
+      await waitFor(async () => {
+        await TextField({ id: 'interval-field' }).has({ value: '3' });
+      });
+    });
+
+    test('renders the location value as expected', async () => {
+      await waitFor(async () => {
+        // FIXME this isn't great -- would rather use option label if you can figure that out -- or centralised testing. This needs work
+        await Select('Location*').has({ value: '53cf956f-c1df-410b-8bea-27f712cca7c0' });
+      });
+    });
+
+    test('renders the Generate receiving pieces button (enabled)', async () => {
+      await waitFor(async () => {
+        await Button('Generate receiving pieces').has({
+          disabled: false,
+        });
+      });
+    });
+
+    describe('clicking generate button', () => {
+      beforeEach(async () => {
+        await waitFor(async () => {
+          await Button('Generate receiving pieces').click();
+        });
+      });
+
+      // mock.calls[0] is the first call -- we iterate over a lot of calls here so we should test them all (I know this will change)
+      test('submitReceivingIds went to the right endpoint', () => {
+        // mock.calls[0][0] is the first argument (endpoint)
+        expect(mockKy.mock.calls[0][0]).toBe('orders/pieces');
+      });
+
+      // FIXME Jack I have no idea where this info comes from, idk if this is the right stuff to test
+      test('submitReceivingIds sent the right information', () => {
+
+        const firstSendReceivingPiece = mockKy.mock.calls[0][1];
+        // FIXME you can do better than this Jack -- proof of concept
+        expect(firstSendReceivingPiece.json.receiptDate?.toString()).toEqual('Fri Oct 04 2024 01:00:00 GMT+0100 (British Summer Time)');
+
+        // mock.calls[0][1] is the second argument (data)
+        expect(omit(firstSendReceivingPiece, ['json.receiptDate'])).toEqual({
+          json: {
+            displayOnHolding: false,
+            displayToPublic: false,
+            locationId: '53cf956f-c1df-410b-8bea-27f712cca7c0',
+            displaySummary: '5 9',
+            format: 'Physical',
+            poLineId: 'baec48dd-1594-2712-be8f-de336bc83fcc',
+            supplement: false,
+            titleId: '7cef39f1-4fb1-49d5-9a6b-a072e632144d'
+          }
+        });
+      });
     });
   });
 });
