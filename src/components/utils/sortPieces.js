53d44f6d3725f2d7ce25ca00371c7ce35e49c559
Jack Golding <jack.golding@k-int.com>
2025-03-18 14:52:09 +0000

    refactor: Sort pieces nested operation
    Seperated out operation within sortPieces util function

--- src/components/utils/sortPieces.js
+++ src/components/utils/sortPieces.js
@@ -1,5 +1,17 @@
 import { INTERNAL_COMBINATION_PIECE } from '../../constants/internalPieceClasses';
 
+const comparePieces = (a, b) => {
+  // Sort all pieces by date or by the recurrence pieces of a combination piece);
+  return (a?.class === INTERNAL_COMBINATION_PIECE
+    ? a?.recurrencePieces[0]?.date
+    : a?.date) <
+    (b?.class === INTERNAL_COMBINATION_PIECE
+      ? b?.recurrencePieces[0]?.date
+      : b?.date)
+    ? -1
+    : 1;
+};
+
 // Used to sort pieces by date and combination pieces by their recurrence pieces date
 const sortPieces = (pieces) => {
   // Itereate through the pieces and find combination pieces
@@ -13,21 +25,7 @@ const sortPieces = (pieces) => {
         }
         : p;
     })
-    .sort((a, b) => {
-      // Sort all pieces by date or by the recurrence pieces of a combination piece
-      return (a?.class === INTERNAL_COMBINATION_PIECE
-        ? a?.recurrencePieces[0]?.date
-        : a?.date) <
-        (b?.class === INTERNAL_COMBINATION_PIECE
-          ? b?.recurrencePieces[0]?.date
-          : b?.date)
-        ? -1
-        : 1;
-    });
+    .sort((a, b) => comparePieces(a, b));
 };
-
-const comparePieces = (a, b) => {
-  return a?.date < b?.date ? -1 : 1;
-}
 
 export default sortPieces;
