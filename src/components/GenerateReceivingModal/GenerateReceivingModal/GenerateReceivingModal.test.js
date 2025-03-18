0517dfd265229aeb3f976159f2370ff8b68ff3aa
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2025-03-18 11:28:10 +0000

    test: Added proof of concept to up coverage on GenerateReceivingPiecesModal (#375)
    * test: Added proof of concept to up coverage on GenerateReceivingPiecesModal
    
    * test: Pieces batch endpoint
    
    Tweaked test since we now use pieces-batch endpoint
    
    * test: Adjusted tests
    
    Adjusted already written tests to focus on information that matters for the first and second call
    
    * test: Fixed timezeon issues
    
    Fixed an issue in which different machines would have different timezeons, causing tests to fail
    
    ---------
    
    Co-authored-by: Jack Golding <94838251+Jack-Golding@users.noreply.github.com>
    Co-authored-by: Jack Golding <jack.golding@k-int.com>

--- src/components/GenerateReceivingModal/GenerateReceivingModal/GenerateReceivingModal.test.js
+++ src/components/GenerateReceivingModal/GenerateReceivingModal/GenerateReceivingModal.test.js
@@ -1,8 +1,19 @@
 import { useState } from 'react';
 import { useMutation } from 'react-query';
+import omit from 'lodash/omit';
 
-import { renderWithIntl, Button, Modal } from '@folio/stripes-erm-testing';
+import {
+  renderWithIntl,
+  Button,
+  Modal,
+  Select,
+  TextField,
+} from '@folio/stripes-erm-testing';
 
+// EXAMPLE reading the output from mockKy
+import { mockKy } from '@folio/stripes/core';
+
+import { waitFor } from '@folio/jest-config-stripes/testing-library/react';
 import GenerateReceivingModal from './GenerateReceivingModal';
 
 import { translationsProperties } from '../../../../test/helpers';
@@ -19,38 +30,15 @@ jest.mock('../GenerateReceivingModalInfo', () => () => (
 
 jest.mock('@folio/stripes-acq-components', () => ({
   ...jest.requireActual('@folio/stripes-acq-components'),
-  useInstanceHoldings: () => ({ isLoading: false, data: mockHoldings }),
-  useLocations: () => ({ isLoading: false, data: mockLocations }),
+  useInstanceHoldingsQuery: () => ({
+    isLoading: false,
+    holdings: mockHoldings,
+  }),
+  useLocationsQuery: () => ({ isLoading: false, locations: mockLocations }),
 }));
 
-/* EXAMPLE Mocking useMutation to allow us to test the .then clause */
-// Setting up jest fn here to test paramters passed in by component
-const mockPreview = jest.fn(() => Promise.resolve(true));
-const mockGenerate = jest.fn(() => Promise.resolve(true));
-jest.mock('react-query', () => {
-  const { mockReactQuery } = jest.requireActual('@folio/stripes-erm-testing');
-
-  return {
-    ...jest.requireActual('react-query'),
-    ...mockReactQuery,
-    useMutation: jest.fn((key, func) => ({
-      mutateAsync: (...incomingParams) => {
-        // Actually call function coming from component
-        // This assumes that ky has been mocked, which it should have been by __mocks__ stripes-core.
-
-        // If this function was async, we might need to do something different.
-        // As it is, it's a synchronous call to ky which returns a promise we then chain on.
-        func();
-
-        // Ensure we return the promise resolve from above, so that any _subsequent_ .then calls can flow
-        if (key.includes('submitReceivingPiece')) {
-          return mockPreview(...incomingParams);
-        }
-        return mockGenerate(...incomingParams);
-      },
-    })),
-  };
-});
+// Option to be selected in 'Location' dropdown
+const selectedLocationOption = mockLocations.find((l) => l.name === 'Annex');
 
 const TestComponent = () => {
   // We need actual state in here for close test
@@ -82,19 +70,108 @@ describe('GenerateReceivingModal', () => {
     await Modal('Generate receiving pieces').exists();
   });
 
-  // test('renders the GenerateReceivingModalInfo component', () => {
-  //   const { getByText } = renderComponent;
-  //   expect(getByText('GenerateReceivingModalInfo')).toBeInTheDocument();
-  // });
+  test('renders the Generate receiving pieces button (disabled)', async () => {
+    await waitFor(async () => {
+      await Button('Generate receiving pieces').has({
+        disabled: true,
+      });
+    });
+  });
 
-  // test('renders the GenerateReceivingModalInfo component', () => {
-  //   const { getByText } = renderComponent;
-  //   expect(getByText('GenerateReceivingModalForm')).toBeInTheDocument();
-  // });
+  describe('filling out generate modal form', () => {
+    beforeEach(async () => {
+      // Fill out fake value to enable button
+      await waitFor(async () => {
+        await TextField({ id: 'interval-field' }).fillIn('3');
+        await Select('Location*').chooseAndBlur(selectedLocationOption?.name);
+      });
+    });
 
-  test('renders the Generate receiving pieces button', async () => {
-    await Button('Generate receiving pieces').has({
-      disabled: true,
+    test('renders the filled value as expected', async () => {
+      await waitFor(async () => {
+        await TextField({ id: 'interval-field' }).has({ value: '3' });
+      });
+    });
+
+    test('renders the location value as expected', async () => {
+      await waitFor(async () => {
+        // Switch this from using hardcoded Id to using find on mockLocations
+        await Select('Location*').has({
+          value: selectedLocationOption.id,
+        });
+      });
+    });
+
+    test('renders the Generate receiving pieces button (enabled)', async () => {
+      await waitFor(async () => {
+        await Button('Generate receiving pieces').has({
+          disabled: false,
+        });
+      });
+    });
+
+    describe('clicking generate button', () => {
+      beforeEach(async () => {
+        await waitFor(async () => {
+          await Button('Generate receiving pieces').click();
+        });
+      });
+
+      // Since we're now using the order/parts endpoint, should only be making one call to orders
+      test('submitReceivingPieces went to the right endpoint', () => {
+        // mock.calls[0][0] is the first argument (endpoint)
+        const endpointCalled = mockKy.mock.calls[0][0];
+        expect(endpointCalled).toBe('orders/pieces-batch');
+      });
+
+      test('submitReceivingPieces sent the right information', () => {
+        const firstReceivingPiece = mockKy.mock.calls[0][1]?.json?.pieces?.[0];
+
+        // First thing to check is that display summary is correct
+        expect(firstReceivingPiece.displaySummary?.toString()).toEqual('5 9');
+        // Secondly, check the recieptDate is the piece date adjusted by interval
+        // In this case its 2024-10-01 + 3 days
+        // We're using toLocaleDateString to avoid timezone issues when running on different environments
+        // Should be ok since piece date is always at midnight, will want a refactor when we move to dayjs
+        expect(
+          new Date(firstReceivingPiece.receiptDate).toLocaleDateString('en')
+        ).toEqual('10/4/2024');
+
+        // Check the rest of the values, values that are taken from the serial, form values and defaults
+        // Id is somewhat irrelevant, since we actually want to make sure the same one is being passed to the next call
+        expect(
+          omit(firstReceivingPiece, ['receiptDate', 'id', 'displaySummary'])
+        ).toEqual({
+          // Defined by serial
+          poLineId: serial?.orderLine?.remoteId,
+          titleId: serial?.orderLine?.titleId,
+          // Selected form values
+          locationId: selectedLocationOption.id,
+          // Defaults
+          displayOnHolding: false,
+          displayToPublic: false,
+          format: 'Physical',
+          supplement: false,
+        });
+      });
+
+      test('submitReceivingIds went to the right endpoint', () => {
+        // mock.calls[1][0] is the first argument (endpoint)
+        const endpointCalled = mockKy.mock.calls[1][0];
+        expect(endpointCalled).toBe(
+          `serials-management/predictedPieces/${pieceSet.id}`
+        );
+      });
+
+      test('submitReceivingIds sent the right information', () => {
+        const firstInternalPiece = mockKy.mock.calls[1][1]?.json?.pieces?.[0];
+
+        // Check that the receivingId is the same as the one generated in the first call
+        // This is the only real important thing that matters wth regards to this call
+        expect(
+          firstInternalPiece.receivingPieces?.[0]?.receivingId?.toString()
+        ).toEqual(mockKy.mock.calls[0][1]?.json?.pieces?.[0]?.id?.toString());
+      });
     });
   });
 });
