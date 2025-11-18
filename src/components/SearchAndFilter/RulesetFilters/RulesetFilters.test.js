d6284ff3c0acdf14d4d784379da79012edd0691a
MonireRasouli <monireh.rasouli@k-int.co.uk>
2025-11-18 11:37:20 +0000

    add tests for RulesetFilters

---
+++ src/components/SearchAndFilter/RulesetFilters/RulesetFilters.test.js
@@ -0,0 +1,73 @@
+import {
+  Accordion,
+  renderWithIntl,
+  Checkbox,
+} from '@folio/stripes-erm-testing';
+import { screen } from '@folio/jest-config-stripes/testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { translationsProperties } from '../../../../test/helpers';
+import { refdata as mockRefdata } from '../../../../test/resources';
+
+import RulesetFilters from './RulesetFilters';
+
+jest.mock('../../utils', () => ({
+  ...jest.requireActual('../../utils'),
+  useSerialsManagementRefdata: () => mockRefdata.filter((rdc) => rdc.desc === 'ModelRuleset.ModelRulesetStatus'),
+}));
+
+const activeFilters = {
+  modelRulesetStatus: ['active'],
+};
+
+const stateMock = jest.fn();
+
+const filterHandlers = {
+  state: stateMock,
+  checkbox: () => {},
+  clear: () => {},
+  clearGroup: () => {},
+  reset: () => {},
+};
+
+describe('RulesetFilters', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <RulesetFilters
+          activeFilters={activeFilters}
+          filterHandlers={filterHandlers}
+        />
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  test('renders the Status accordion', async () => {
+    screen.debug();
+    await Accordion('Status').exists();
+  });
+
+  test('renders the expected Active label', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Active')).toBeInTheDocument();
+  });
+
+  test('renders the expected Closed label', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Closed')).toBeInTheDocument();
+  });
+
+  test('renders Active Checkbox', async () => {
+    await Checkbox({ id: 'clickable-filter-modelRulesetStatus-active' }).exists();
+  });
+
+  test('renders Closed Checkbox', async () => {
+    await Checkbox({ id: 'clickable-filter-modelRulesetStatus-closed' }).exists();
+  });
+
+  test('clicking the Ruleset checkbox', async () => {
+    await Checkbox('Active').is({ checked: true });
+    await Checkbox('Closed').is({ checked: false });
+  });
+});
