f2db5c523517a3b4739b75751f125b8c0d6b6de0
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-12-17 11:21:37 +0100

    * extend test

--- src/routes/TemplateCreateRoute/TemplateCreateRoute.test.js
+++ src/routes/TemplateCreateRoute/TemplateCreateRoute.test.js
@@ -5,6 +5,16 @@ import { translationsProperties } from '../../../test/helpers';
 import TemplateCreateRoute from './TemplateCreateRoute';
 import * as utils from '../../components/utils';
 
+let capturedFormProps;
+
+jest.mock('react-final-form', () => ({
+  Form: (props) => {
+    capturedFormProps = props;
+    const handleSubmit = (values) => props.onSubmit(values);
+    return props.children({ handleSubmit });
+  },
+}));
+
 const mockMutateAsync = jest.fn();
 
 jest.mock('react-query', () => {
@@ -37,6 +47,7 @@ jest.mock('../../components/utils', () => ({
     templateView: jest.fn((id) => `/serials-management/modelRulesets/${id}`),
     templates: jest.fn(() => '/serials-management/modelRulesets'),
   },
+  getRulesetFormValues: jest.fn(() => ({ fromRuleset: 'yes' })),
   rulesetSubmitValuesHandler: jest.fn(() => ({ fake: 'serialRuleset' })),
 }));
 
@@ -55,6 +66,7 @@ describe('TemplateCreateRoute', () => {
   beforeEach(() => {
     mockMutateAsync.mockClear();
     utils.rulesetSubmitValuesHandler.mockClear();
+    utils.getRulesetFormValues.mockClear();
     utils.urls.templateView.mockClear();
     utils.urls.templates.mockClear();
     mockHistoryPush.mockClear();
@@ -76,7 +88,9 @@ describe('TemplateCreateRoute', () => {
   test('calls submitTemplate with mapped values and uses rulesetSubmitValuesHandler', async () => {
     expect(mockLatestHandlers).toBeDefined();
 
-    await mockLatestHandlers.onSubmit({});
+    await mockLatestHandlers.onSubmit({
+      rulesetStatus: { value: 'active' },
+    });
 
     expect(utils.rulesetSubmitValuesHandler).toHaveBeenCalledTimes(1);
 
@@ -109,5 +123,47 @@ describe('TemplateCreateRoute', () => {
 
     expect(utils.urls.templateView).toHaveBeenCalledWith('1234');
     expect(mockHistoryPush).toHaveBeenCalledWith('/serials-management/modelRulesets/1234?filters=modelRulesetStatus.active');
+  });
+
+  test('sets initialValues to rulesetStatus active when not copying', () => {
+    expect(capturedFormProps).toBeDefined();
+    expect(capturedFormProps.initialValues).toEqual({
+      rulesetStatus: { value: 'active' },
+    });
+  });
+
+  test('sets initialValues from copyFrom template + serialRuleset', () => {
+    mockLocation.state = {
+      copyFrom: {
+        name: 'My Template',
+        description: 'Copied description',
+        exampleLabel: 'Vol. 1',
+        serialRuleset: { some: 'ruleset' },
+      },
+    };
+
+    utils.getRulesetFormValues.mockReturnValueOnce({
+      rulesetStatus: { value: 'active' },
+      recurrence: { period: '3' },
+    });
+
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <TemplateCreateRoute />
+      </MemoryRouter>,
+      translationsProperties
+    );
+
+    expect(utils.getRulesetFormValues).toHaveBeenCalledWith({ some: 'ruleset' });
+
+    expect(capturedFormProps.initialValues).toEqual(
+      expect.objectContaining({
+        rulesetStatus: { value: 'active' },
+        recurrence: { period: '3' },
+        name: 'Copy of: My Template',
+        modelRulesetDescription: 'Copied description',
+        exampleLabel: 'Vol. 1',
+      })
+    );
   });
 });
